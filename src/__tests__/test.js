bf46c2be0d1dfb645c201961c3f06a581f9051bc
Dmytro Tsehelnyk <dtsehelnyk@cybervisiontech.com>
2021-11-09 15:16:15 +0200

    solution

---
+++ src/__tests__/test.js
@@ -0,0 +1,5 @@
+'use strict';
+
+test('', () => {
+  expect(1).toBe(1);
+});
--- src/app.js
+++ src/app.js
@@ -1,1 +1,19 @@
+/* eslint-disable no-console */
 'use strict';
+
+const fs = require('fs');
+const { exit } = require('process');
+
+fs.copyFile(
+  './src/file.txt',
+  './src/destination/file.txt',
+  fs.constants.COPYFILE_EXCL,
+  (error) => {
+    if (error) {
+      console.log(error);
+      exit();
+    }
+
+    console.log('Successfully copied!');
+  }
+);
