c0b2c58dd1bc8568e2adc8b410dbe7bdda932801
Lena Kravchenko <cupp.lenashapoval@gmail.com>
2021-11-23 23:08:15 +0200

    Copy files solution added

--- src/app.js
+++ src/app.js
@@ -1,1 +1,36 @@
 'use strict';
+
+import readline from 'readline';
+import fs from 'fs';
+
+const terminal = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+terminal.question("Input the command in format: 'cp fromFileName toFileName'\n", (input) => {
+  const words = input.split(' ');
+
+  if (words.length === 3 && words[0] === 'cp') {
+    fs.readFile(words[1], (err, content) => {
+      if (err) {
+        throw err;
+        terminal.close();
+      }
+
+      fs.writeFile(words[2], content, (err, data) => {
+        if (err) {
+          throw err;
+          terminal.close();
+        }
+
+        console.log("Success.");
+        terminal.close();
+      });
+    });
+  } else {
+    console.log("Please, check the input data");
+    terminal.close();
+  }
+})
+
