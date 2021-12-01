b43ef3f686ccc2f4a130dc23e86e45dcfc606e55
Lena Kravchenko <cupp.lenashapoval@gmail.com>
2021-12-01 14:19:57 +0200

    Fixes-2 after review

--- src/app.js
+++ src/app.js
@@ -1,42 +1,25 @@
 'use strict';
 
-import readline from 'readline';
 import fs from 'fs';
 
-const terminal = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+const [ fileFrom, fileTo ] = [...process.argv.slice(3)];
 
-terminal.question("Input the command in format: 'cp fromFileName toFileName'\n", (input) => {
-  const words = input.split(' ');
-
-  if (words.length === 3 && words[0] === 'cp') {
-    fs.readFile(words[1], (err, content) => {
-      if (err) {
-        throw err;
-        terminal.close();
-      }
-
-      if (words[1] === words[2]) {
-        console.log("The same directory");
-        terminal.close();
-        return;
-      }
-
-      fs.writeFile(words[2], content, (err, data) => {
-        if (err) {
-          throw err;
-          terminal.close();
-        }
-
-        console.log("Success.");
-        terminal.close();
-      });
-    });
-  } else {
-    console.log("Please, check the input data");
-    terminal.close();
+fs.readFile(fileFrom, (err, content) => {
+  if (err) {
+    throw err;
   }
-})
+
+  if (fileFrom === fileTo) {
+    console.log("The same location");
+    return;
+  }
+
+  fs.writeFile(fileTo, content, (err, data) => {
+    if (err) {
+      throw err;
+    }
+
+    console.log("Success. File was copied.");
+  });
+});
 
