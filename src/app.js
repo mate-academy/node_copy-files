e0d312c9e5c187a44d3bfdcc56d627055ae9e1e7
Oleg Semko <oleg.semko@zazmic.com>
2021-12-18 09:43:34 +0200

    copy file

--- src/app.js
+++ src/app.js
@@ -1,1 +1,28 @@
 'use strict';
+
+/* eslint-disable no-console */
+
+const fs = require('fs');
+
+const [source, destination] = process.argv.slice(2);
+
+fs.readFile(source, (err, content) => {
+  if (err) {
+    throw err;
+  }
+
+  console.log(content);
+
+  if (source === destination) {
+    console.log('Can not copy to the same location');
+
+    return;
+  }
+
+  fs.writeFile(destination, content, (error, data) => {
+    if (error) {
+      throw error;
+    }
+    console.log('File copied');
+  });
+});
