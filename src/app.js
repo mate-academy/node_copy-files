3e1228421087bf2148c0acc714ad3974d6d6d745
Valerie Yaskevych <redrum.back@gmail.com>
2021-12-20 05:58:50 +0200

    solved

--- src/app.js
+++ src/app.js
@@ -1,1 +1,27 @@
+/* eslint-disable no-console */
 'use strict';
+
+const fs = require('fs');
+
+let fileContent;
+const fileName = process.argv[2];
+const newFileName = process.argv[3];
+
+if (fileName === newFileName) {
+  console.log('Can\'t copy to the same location');
+
+  return;
+}
+
+try {
+  fileContent = fs.readFileSync(`src/${fileName}`, 'utf8');
+} catch (error) {
+  throw error;
+}
+
+try {
+  fs.writeFileSync(`src/${newFileName}`, fileContent);
+  console.log('File was successfully copied');
+} catch (error) {
+  throw error;
+}
