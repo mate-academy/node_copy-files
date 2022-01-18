983e65b6db1a01de5a61febeb23dca34082cd92e
Roman <65686226+morozit@users.noreply.github.com>
2022-01-18 11:08:14 +0200

    develop

--- src/app.js
+++ src/app.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const fs = require('fs');
+
+const [fileFrom, fileName] = process.argv.slice(2);
+
+fs.copyFile(`${fileFrom}`, `./src/destination/${fileName}`, (error) => {
+  // eslint-disable-next-line no-console
+  console.log('compleat');
+
+  if (error) {
+    // eslint-disable-next-line no-console
+    console.log(error);
+    // eslint-disable-next-line no-console
+    console.log('Ви намагається скопіювати в те саме місце');
+  }
+});
