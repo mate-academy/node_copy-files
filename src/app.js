f7750cb27bb61f835ef2654e5748e9ed2d734759
Roman Moroz <65686226+morozit@users.noreply.github.com>
2022-01-19 21:28:32 +0200

    develop v0.3

--- src/app.js
+++ src/app.js
@@ -2,21 +2,34 @@
 
 const fs = require('fs');
 
-const [fileFrom, fileName] = process.argv.slice(2);
+const [fileFrom, fileTo] = process.argv.slice(2);
 
-fs.copyFile(`${fileFrom}`, `${fileName}`, (error) => {
+if (!fileFrom.includes('/') && !fileTo.includes('/')) {
+  // eslint-disable-next-line no-console
+  console.log('Ви намагається скопіювати в те саме місце');
+
+  return
+}
+
+const destArrFrom = fileFrom.split('/');
+const nameFileFrom = destArrFrom[0];
+const destArr = fileTo.split('/');
+const nameFileTo = destArr[0];
+
+
+if (nameFileFrom === nameFileTo) {
+  // eslint-disable-next-line no-console
+  console.log('Ви намагається скопіювати в те саме місце');
+
+  return;
+}
+
+fs.copyFile(`${fileFrom}`, `${fileTo}`, (error) => {
   if (error) {
     // eslint-disable-next-line no-console
     console.log(error);
     // eslint-disable-next-line no-console
     console.log('щось пішло не так');
-
-    return;
-  }
-
-  if (fileFrom === fileName) {
-    // eslint-disable-next-line no-console
-    console.log('Ви намагається скопіювати в те саме місце');
 
     return;
   }
