08efa6beb0f96f1dced6cfeab3c469f3135b57b2
Roman <65686226+morozit@users.noreply.github.com>
2022-01-19 13:17:49 +0200

    develop v0.2

--- src/app.js
+++ src/app.js
@@ -4,14 +4,23 @@ const fs = require('fs');
 
 const [fileFrom, fileName] = process.argv.slice(2);
 
-fs.copyFile(`${fileFrom}`, `./src/destination/${fileName}`, (error) => {
-  // eslint-disable-next-line no-console
-  console.log('compleat');
-
+fs.copyFile(`${fileFrom}`, `${fileName}`, (error) => {
   if (error) {
     // eslint-disable-next-line no-console
     console.log(error);
     // eslint-disable-next-line no-console
-    console.log('Ви намагається скопіювати в те саме місце');
+    console.log('щось пішло не так');
+
+    return;
   }
+
+  if (fileFrom === fileName) {
+    // eslint-disable-next-line no-console
+    console.log('Ви намагається скопіювати в те саме місце');
+
+    return;
+  }
+
+  // eslint-disable-next-line no-console
+  console.log('compleat');
 });
