daff5036a0e8ae484b2ecf63f7579f018824dd88
dant0n <dmibuc@gmail.com>
2022-04-02 15:47:26 +0300

    Finish hw.

--- src/app.js
+++ src/app.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const fs = require('fs');
+const path = require('path');
+
+const [source, destination] = process.argv.slice(2);
+
+copyFile(source, destination);
+
+function copyFile(sourceName, destinationName) {
+  if (path.dirname(sourceName) === path.dirname(destinationName)) {
+    return;
+  }
+
+  fs.copyFile(sourceName, destinationName, err => {
+    if (err) {
+      throw err;
+    }
+  });
+}
