9b5afbb8f10f5f1c855c3bc1037eef0a2d723701
Taras Pyda <tapac1991@icloud.com>
2022-05-22 12:35:26 +0300

    Solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,12 @@
+/* eslint-disable no-console */
 'use strict';
+
+const fs = require('fs');
+
+const [,, operation, file, copy] = process.argv;
+
+if (operation === 'cp' && file !== copy) {
+  fs.readFile(`./src/${file}`, 'utf-8', (_, data) => {
+    fs.writeFile(`./src/${copy}`, data, () => {});
+  });
+};
