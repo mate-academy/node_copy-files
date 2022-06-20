318223cdbd004e1b696d6427071e55cdde65165f
Dima Permiakov <permiakov.d.s@gmail.com>
2022-06-20 11:56:28 +0300

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const readline = require('readline');
+const fs = require('fs');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question('Where to copy the file?', (userInput) => {
+  fs.copyFile('src/copy.js', `src/${userInput}/copy.js`,
+    fs.constants.COPYFILE_EXCL, (err) => {
+      if (err) {
+        // eslint-disable-next-line no-console
+        console.log(err);
+      }
+      // eslint-disable-next-line no-console
+      console.log('Success');
+    });
+
+  rl.close();
+});
