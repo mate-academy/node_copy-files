1d9fc843db1aa091cac2dcd79cfbb444dcf05c8d
Alisa Ivashkevych <a.ivashkevych@mate.academy>
2022-07-05 18:00:13 +0300

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,37 @@
 'use strict';
+
+const fs = require('fs');
+const readline = require('readline');
+
+const terminal = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+terminal.question('Please specify the output file: ', (outputFile) => {
+  terminal.question('Please specify the input file: ', (inputFile) => {
+
+    if (outputFile === inputFile) {
+      // eslint-disable-next-line
+      console.log('You\'re trying to copy file to itself. Please try again.');
+
+      terminal.close();
+
+      return;
+    }
+
+    fs.copyFile(outputFile, inputFile, (err) => {
+      if (err) {
+        // eslint-disable-next-line
+        console.log(err);
+
+        return;
+      }
+
+      // eslint-disable-next-line
+      console.log(`File '${outputFile}' was successfuly copied to '${inputFile}'!`);
+    });
+
+    terminal.close();
+  });
+});
