4cdc799787ec5b791d18a635db64ca83a89d7049
Vitalii Nepomiashchyi <nepomyaschyi1@gmail.com>
2022-07-11 21:44:37 +0300

    Solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,24 @@
+/* eslint-disable no-console */
 'use strict';
+
+const fs = require('fs');
+
+const args = process.argv.slice(2);
+const [fileLocation, newLocation] = args;
+const fileName = fileLocation.split('/').pop();
+
+const writeFile = (data) => {
+  fs.writeFile(`${newLocation}/${fileName}`, data, (err) => {
+    if (err) {
+      console.log(err);
+    }
+  });
+};
+
+fs.readFile(fileName, 'utf-8', (err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    writeFile(data);
+  }
+});
