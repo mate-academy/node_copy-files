2ff2274085e7918289c8b7d76c2709268d12ff03
Bohdana Usenko <usenkobogdana19@gmail.com>
2022-08-08 23:41:51 +0300

    copy files

--- src/app.js
+++ src/app.js
@@ -1,1 +1,24 @@
 'use strict';
+
+const fs = require('fs');
+
+const [firstFile, secondFile] = process.argv.slice(2);
+
+if (firstFile === secondFile) {
+  // eslint-disable-next-line no-console
+  console.log('You\'re trying to copy file to itself.');
+
+  return;
+}
+
+fs.copyFile(`./src/${firstFile}`, `./src/${secondFile}`, (err) => {
+  if (err) {
+    // eslint-disable-next-line no-console
+    console.log(err);
+
+    return;
+  }
+
+  // eslint-disable-next-line no-console
+  console.log(`File ${firstFile} was successfully copied to ${secondFile}`);
+});
