8774cccff245f6f17163581b611ac3a981d72864
Roman Kovalchuk <roma220702@gmail.com>
2022-09-11 12:43:49 +0300

    Solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,26 @@
+/* eslint-disable no-console */
 'use strict';
+
+const fs = require('fs');
+const rl = require('readline');
+
+const terminal = rl.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+terminal.question('Enter: Address ', (data) => {
+  const [ filePath, copyesPath ] = data.trim().split(' ');
+
+  terminal.close();
+
+  if (filePath !== copyesPath) {
+    fs.copyFile(filePath, copyesPath, (error) => {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log('Succesfully');
+      }
+    });
+  }
+});
