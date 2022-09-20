ecc2507a968a33f1ab07db7ead1b8fce3c6e5156
Pavlo <Pmoroz2004@gmail.com>
2022-09-20 16:39:11 +0300

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,52 @@
+/* eslint-disable no-console */
 'use strict';
+
+const readline = require('readline');
+const fs = require('fs');
+
+const terminal = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+terminal.question(
+  'enter path to your file and his name ',
+  (input) => {
+    const [ path, name ] = input.trim().split(' ');
+
+    const file = new Promise((resolve, reject) => {
+      fs.readFile(
+        `${path}/${name}`,
+        'utf8',
+        (err, data) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(data);
+          }
+        }
+      );
+    });
+
+    terminal.question(`enter new path `, (newPath) => {
+      file
+        .then(data => {
+          fs.writeFile(
+            `${newPath}/${name}`,
+            data,
+            (err) => {
+              if (err) {
+                console.log(err);
+              }
+            },
+          );
+
+          console.log(`\nNew file is exists in ${newPath} directory`);
+          terminal.close();
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    });
+  },
+);
