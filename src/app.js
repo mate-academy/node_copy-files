699eb3c0a1c878be931b9b9b82615a77a42c1c39
sergii2705 <maniurov_sv@ukr.net>
2022-09-28 16:09:03 +0300

    Solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,18 @@
 'use strict';
+/* eslint-disable no-console */
+
+const fs = require('fs/promises');
+
+const [command, inputFile, outputFile] = process.argv.slice(2, 5);
+
+if (command !== 'cp') {
+  console.log('\x1b[31m', `Invalid command - ${command}, try to use <cp>`);
+} else if (inputFile !== outputFile) {
+  fs.copyFile(inputFile, outputFile)
+    .then(() => {
+      console.log('\x1b[32m', 'File copied successfully');
+    })
+    .catch((err) => {
+      console.log('\x1b[31m', `failed to copy file, ${err}`);
+    });
+}
