69bc89605d2cd8eb2ed9dcfe4386ad37527b9837
Roman Nikitenko <Lomkaaa@ukr.net>
2022-10-12 08:03:13 +0300

    Task complete

--- src/app.js
+++ src/app.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const fs = require('fs');
+const fileSource = process.argv.slice(2);
+
+if (fileSource[0] === 'cp') {
+  fs.copyFile(`./${fileSource[1]}`, `${fileSource[2]}`, (err) => {
+    if (err) {
+      // eslint-disable-next-line no-console
+      console.log(`File not find ${fileSource[1]}`);
+
+      return;
+    }
+
+    // eslint-disable-next-line no-console
+    console.log('File copied');
+  });
+}
