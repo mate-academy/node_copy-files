d8a76f08c1a6b8fcc6b4e2d8efbcb5f2bc08d97b
Max Kravchenko <max.kravchenko@gmail.com>
2022-10-13 21:23:06 +0300

    Create node app for copying files

--- src/app.js
+++ src/app.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const fs = require('fs/promises');
+const path = require('path');
+
+const source = process.argv[2];
+const destination = process.argv[3];
+
+try {
+  fs.copyFile(path.join(__dirname, source), path.join(__dirname, destination));
+} catch (err) {
+  throw new Error('blo');
+}
