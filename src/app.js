14b9e466a2456b3e87216eea7b63aceb3d530190
Roman Nikitenko <Lomkaaa@ukr.net>
2022-10-13 20:25:58 +0300

    Fix

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ if (fileSource[0] === 'cp') {
   fs.copyFile(`./${fileSource[1]}`, `${fileSource[2]}`, (err) => {
     if (err) {
       // eslint-disable-next-line no-console
-      console.log(`File not find ${fileSource[1]}`);
+      console.log(`File not found ${fileSource[1]}`);
 
       return;
     }
