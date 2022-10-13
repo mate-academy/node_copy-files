8525ebb159d8a2bfa273f05b027485b58e886d3b
Andrii Stupnytskyi <pastol8888@gmail.com>
2022-10-13 21:50:29 +0300

    node copy files

--- src/app.js
+++ src/app.js
@@ -1,1 +1,36 @@
 'use strict';
+
+const { readFile, writeFile, existsSync } = require('fs');
+const [, , source, destination] = process.argv;
+// eslint-disable-next-line no-console
+const log = console.log;
+
+if (!source || !destination) {
+  log('Please provide a source and destination file');
+  process.exit();
+}
+
+if (existsSync(source)) {
+  log('Please provide a valid source file');
+  process.exit();
+}
+
+if (existsSync(destination)) {
+  log('Destination file already exists');
+} else {
+  readFile(source, 'utf8', (error, data) => {
+    if (error) {
+      log('Error reading source file');
+
+      return;
+    }
+
+    writeFile(destination, data, (err) => {
+      if (err) {
+        log('Error writing destination file');
+      } else {
+        log('File copied successfully');
+      }
+    });
+  });
+}
