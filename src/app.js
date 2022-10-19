66f93e1db4d78ef1bdc80b0fdafe691b93aeb9b5
Roma Artiukh <romanskarb@ukr.net>
2022-10-19 14:28:56 +0300

    some fixes were added

--- src/app.js
+++ src/app.js
@@ -3,31 +3,24 @@
 
 const fs = require('fs');
 
-// fs.copyFile('./src/file.txt', './src/new_file.txt', error => {
-//   if (error) {
-//     throw error('There is not file in this directory');
-//   }
-//    console.log('File was coppied succesfull');
-// });
+const [copyFrom, copyTo] = process.argv.slice(2);
 
-const [prev, next] = process.argv.slice(2);
-
-if (prev === next) {
+if (copyFrom === copyTo) {
   console.log('Can`t copy to the exact location');
 
   return;
 }
 
-fs.readFile(prev, 'utf-8', (errorRead, data) => {
+fs.readFile(copyFrom, 'utf-8', (errorRead, data) => {
   if (errorRead) {
-    console.log(errorRead);
+    console.error(errorRead);
 
     return;
   }
 
-  fs.writeFile(next, data, 'utf-8', errorWrite => {
+  fs.writeFile(copyTo, data, 'utf-8', errorWrite => {
     if (errorWrite) {
-      console.log(errorWrite);
+      console.error(errorWrite);
 
       return;
     }
