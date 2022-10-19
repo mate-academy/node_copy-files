40bca6892a94fea63e936dc2858828a0eea9e393
Kate <krasnova2912@gmail.com>
2022-10-19 16:00:57 +0300

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,33 @@
+/* eslint-disable no-console */
 'use strict';
+
+const inquirer = require('inquirer');
+const fs = require('fs/promises');
+
+const commandHandler = async() => {
+  const command = await inquirer
+    .prompt([{
+      name: 'command',
+      type: 'input',
+      message: 'i am waiting for your command',
+      validate: (line) => line.split(' ').length === 3,
+    }]);
+
+  const commandElements = command.command.split(' ');
+
+  if (commandElements[0] !== 'cp') {
+    console.log('you entered a wrong command');
+  } else if (commandElements[1] === commandElements[2]) {
+    console.log('you are trying to copy the same file');
+  } else {
+    try {
+      const file = await fs.readFile(commandElements[1], 'utf-8');
+
+      fs.writeFile(commandElements[2], file);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+};
+
+commandHandler();
