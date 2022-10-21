7dd8680c99152bb6d5da91d5598cd7576c47c5ce
Anatolii Vlasiuk <vlasyukalid@gmail.com>
2022-10-21 13:03:02 +0300

    Some fixies

--- src/app.js
+++ src/app.js
@@ -6,21 +6,19 @@ const fs = require('fs');
 const [comName, sourse, destination] = process.argv.slice(2);
 
 if (comName === 'copy') {
-
   if (sourse === destination) {
     console.log('You try copy to the same location');
     process.exit();
   }
 
-  fs.readFile('./src/test.txt', 'utf8', (err, data) => {
-
+  fs.readFile(sourse, 'utf8', (err, data) => {
     if (err) {
       console.log(err);
 
       return;
     }
 
-    fs.writeFile('./src/test-copy.txt', data, () => {});
+    fs.writeFile(destination, data, () => {});
   });
 } else {
   console.log('Wrong command name');
