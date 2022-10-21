c491766550b99732539674eb8d8a43ad9d4089e5
Anatolii Vlasiuk <vlasyukalid@gmail.com>
2022-10-21 12:59:38 +0300

    Done

--- src/app.js
+++ src/app.js
@@ -1,1 +1,27 @@
+/* eslint-disable no-console */
 'use strict';
+
+const fs = require('fs');
+
+const [comName, sourse, destination] = process.argv.slice(2);
+
+if (comName === 'copy') {
+
+  if (sourse === destination) {
+    console.log('You try copy to the same location');
+    process.exit();
+  }
+
+  fs.readFile('./src/test.txt', 'utf8', (err, data) => {
+
+    if (err) {
+      console.log(err);
+
+      return;
+    }
+
+    fs.writeFile('./src/test-copy.txt', data, () => {});
+  });
+} else {
+  console.log('Wrong command name');
+}
