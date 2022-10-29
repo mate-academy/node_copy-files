834ecd181e6d41971a1ce6039afffd51b2442df4
Max Bovtun <maxbovtun@gmail.com>
2022-10-29 10:34:57 +0300

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const fs = require('fs');
+const path = require('path');
+
+function cp(fileFrom, fileTo) {
+  if (fileFrom === fileTo) {
+    return;
+  }
+
+  try {
+    const basePath = path.join(__dirname, fileFrom);
+    const content = fs.readFileSync(basePath, 'utf-8');
+
+    fs.writeFileSync(fileTo, content);
+  } catch (e) {
+    throw new Error(e);
+  }
+}
+
+cp('test.txt', 'copy-test.txt');
