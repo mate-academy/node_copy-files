2cf28be1f727c443d4b08dc9db40a03b5ce05d36
Maksym845 <mzuravel845@gmail.com>
2022-11-16 23:15:03 +0200

    solution was added

--- src/app.js
+++ src/app.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const fs = require('fs');
+const path = require('path');
+
+const [filePath, copyPath] = process.argv.slice(2);
+
+function copyFile(initialFile, targetPath) {
+  if (initialFile === targetPath) {
+    return;
+  }
+
+  const initialPath = path.join(__dirname, initialFile);
+  const fileData = fs.readFileSync(initialPath, 'utf-8');
+
+  fs.writeFileSync(targetPath, fileData);
+}
+
+copyFile(filePath, copyPath);
