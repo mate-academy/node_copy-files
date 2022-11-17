42ba32806d8d6c19128531905bbbf89ef6ded8a7
Vlad Malinin <vladmalinin1999@gmail.com>
2022-11-17 14:55:46 +0200

    Create Copy files method

--- src/app.js
+++ src/app.js
@@ -1,1 +1,13 @@
 'use strict';
+const fs = require('fs');
+const path = require('path');
+const [filePath, copyFilePath] = process.argv.slice(2);
+
+const copyFile = (file, copyFile) => {
+  const mainPath = path.join(__dirname, file);
+  const data = fs.readFileSync(mainPath, 'utf-8');
+
+  fs.writeFileSync(copyFile, data);
+}
+
+copyFile(filePath, copyFilePath)
