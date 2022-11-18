02eedba9d048072c23ab518207c26156e2ecf792
Volodymyr Nosachenko <vovanosachenko@meta.ua>
2022-11-18 13:53:30 +0200

    copy file solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,16 @@
+/* eslint-disable no-console */
 'use strict';
+
+const { validation } = require('./validation');
+
+function copyFiles(from, to) {
+  return validation(from, to);
+}
+
+const request = process.argv.slice(2);
+const pathFrom = request[0];
+const pathTo = request[1];
+
+copyFiles(pathFrom, pathTo);
+
+module.exports = { copyFiles };
