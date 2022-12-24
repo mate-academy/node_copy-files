85b388469d8cd7a5e34ed0b34210fd7d457ebf53
Ievgen Volkov <neoodessa86@gmail.com>
2022-12-24 20:36:21 +0200

    Solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,42 @@
 'use strict';
+
+const fs = require('fs');
+
+const [command, src, dest] = process.argv.slice(2);
+
+if (!command || !src || !dest.trim()) {
+  throw new Error('The syntax should be: cp file.txt file-copy.txt');
+}
+
+function copyFile(operation, fromDir, toDir) {
+  const isValid = checkIfValidity(operation, fromDir, toDir);
+
+  if (isValid) {
+    try {
+      fs.copyFileSync(fromDir, toDir);
+    } catch (err) {
+      throw Error(err);
+    }
+  }
+}
+
+function checkIfValidity(operation, fromDir, toDir) {
+  switch (true) {
+    case (operation !== 'cp'):
+      console.log(`\nAny operation here is COPY!\n`);
+
+      return true;
+    case (!fs.existsSync(fromDir)):
+      console.log(`\nFile or folder ${fromDir} does not exist\n`);
+
+      return false;
+    case (fs.existsSync(toDir)):
+      console.log(`\nFile ${toDir} already exists\n`);
+
+      return false;
+    default:
+      return true;
+  }
+}
+
+copyFile(command, src, dest);
