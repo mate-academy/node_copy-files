5c637a70446173506afeb5f0db2e4e5d684a1aa6
Masha <teenderrr@gmail.com>
2022-12-25 11:29:53 +0200

    copy files

--- src/app.js
+++ src/app.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const { copyFiles } = require('./copyFiles.js');
+
+const makeCopy = () => {
+  const path = process.argv.slice(2);
+
+  copyFiles(path[0], path[1]);
+};
+
+module.exports.makeCopy = makeCopy();
