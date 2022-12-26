ff087b834bc19f03ea0a6c20d751c1cb031d6109
Svitlana Yudina <osbka31@gmail.com>
2022-12-26 17:10:00 +0200

    fixes

--- src/app.js
+++ src/app.js
@@ -2,20 +2,9 @@
 'use strict';
 
 const { copyFile } = require('fs');
-const readline = require('readline');
-const terminal = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
 
-terminal.question('What file do you want to copy and where? ', (doublePath) => {
-  const pathArr = doublePath.split(' ');
+const args = process.argv.slice(2);
 
-  copyFile(pathArr[0], pathArr[1], (err) => {
-    if (err) {
-      throw err;
-    }
-    console.log('copying complete!');
-  });
-  terminal.close();
+copyFile(args[0], args[1], () => {
+  console.log('ok');
 });
