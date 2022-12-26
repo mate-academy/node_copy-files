e2e44aaa95d73529839f20bb7aade12d8dbd26c6
Svitlana Yudina <osbka31@gmail.com>
2022-12-26 14:55:42 +0200

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,21 @@
+/* eslint-disable no-console */
 'use strict';
+
+const { copyFile } = require('fs');
+const readline = require('readline');
+const terminal = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+terminal.question('What file do you want to copy and where? ', (doublePath) => {
+  const pathArr = doublePath.split(' ');
+
+  copyFile(pathArr[0], pathArr[1], (err) => {
+    if (err) {
+      throw err;
+    }
+    console.log('copying complete!');
+  });
+  terminal.close();
+});
