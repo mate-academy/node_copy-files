a7fd625077ecae53a6192332c7e27f2c0e5505b0
Viktor Nikolaiev <referee1988@gmail.com>
2022-12-27 02:40:13 +0200

    fix copy to same location

--- src/copyFile.js
+++ src/copyFile.js
@@ -3,6 +3,10 @@
 const { copyFileSync } = require('fs');
 
 function copyFile(src, dest) {
+  if (src === dest) {
+    return;
+  }
+
   try {
     copyFileSync(src, dest);
     process.stdout.write(`${src} was copied to ${dest}`);
