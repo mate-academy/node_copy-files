5a534524403f2300627eec96b5060c11055a4f4b
Ivan Nevmerzhytskyi <nevmerzhytskyi.ivan@gmail.com>
2023-01-14 01:32:09 +0200

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,29 @@
-'use strict';
+/* eslint-disable no-console */
+import fs from 'fs';
+import path from 'path';
+
+const [sourse, destination] = process.argv.slice(2);
+
+function copyFile(initPath, targetPath) {
+  if (path.resolve(path.dirname(initPath)) === path.resolve(targetPath)) {
+    console.log('The file cannot be copied to the same location!!!');
+
+    return;
+  }
+
+  if (fs.existsSync(initPath)) {
+    const target = targetPath + path.sep + path.basename(initPath);
+
+    fs.cp(initPath, target, (error) => {
+      if (!error) {
+        console.log(`${initPath} was copied to ${targetPath}`);
+      } else {
+        console.log('The file cannot be copied to this folder');
+      }
+    });
+  } else {
+    console.log('File does not exist!!!');
+  }
+}
+
+copyFile(sourse, destination);
