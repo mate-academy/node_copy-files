ad312dee5094a05b7bf2923e08095213bdbd5399
Дмитро Ніколенко <dmitronikolenko@MacBook-Air-Dmitro.local>
2023-01-20 15:13:37 +0200

    done!

--- src/app.js
+++ src/app.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const fs = require('fs');
+
+const [source, dest] = process.argv.slice(2);
+
+const copyFile = () => {
+  if (source === dest) {
+    throw Error('Can not copy with the same filename');
+  }
+
+  fs.cp(source, dest, () => {});
+};
+
+copyFile();
