0fb58c33480867b59170191e0cee5d72ecc27634
Matvii Kharchenko <matviy.kharchenko@gmail.com>
2023-01-31 19:43:31 +0100

    s

--- src/app.js
+++ src/app.js
@@ -1,1 +1,18 @@
+/* eslint-disable no-console */
 'use strict';
+
+const fs = require('fs');
+
+const [oldFile, newFile] = process.argv.slice(2);
+
+oldFile === newFile
+  ? fs.copyFile(oldFile, newFile, callback)
+  : console.log('Please, copy to file to another location');
+
+function callback(err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(`${oldFile} was copied to ${newFile}`);
+  }
+}
