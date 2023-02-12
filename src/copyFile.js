ef6aeb08c0d6740345249b864b65359f8a2f42f3
Vadym Voiedilo <asd4s.profile@gmail.com>
2023-02-12 16:22:46 +0200

    Solution

---
+++ src/copyFile.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const fs = require('fs');
+
+const terminal = require('./terminal').terminal;
+const { messages } = require('./messages');
+
+const copyFile = (message) => {
+  terminal.question(message, (recievedCommand) => {
+    const [command, fileToCopy, locationToCopy] = recievedCommand.split(' ');
+
+    const currentLocation = `${__dirname}\\${fileToCopy}`;
+    const destinationLocation = `${__dirname}\\${locationToCopy}`;
+
+    const isSourceExists = fs.existsSync(currentLocation);
+
+    if (command !== 'cd') {
+      copyFile(messages.wrongCommand);
+
+      return;
+    }
+
+    if (!isSourceExists) {
+      copyFile(messages.wrongSource);
+
+      return;
+    }
+
+    if (currentLocation === destinationLocation) {
+      copyFile(messages.wrongLocation);
+
+      return;
+    }
+
+    fs.copyFileSync(currentLocation, destinationLocation);
+
+    /* eslint-disable no-console */
+    console.log('File copied successfully!');
+    terminal.close();
+  });
+};
+
+exports.copyFile = copyFile;
