2c4f9afb9908f381716445b58a47aa0387be6fd2
Artem Hrechuk <grechuck.artem.ag@gmail.com>
2023-02-15 17:22:04 +0200

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const fs = require('fs');
+
+const [pathFromCopy, pathToCopy] = process.argv.slice(2);
+
+function copyFile(pathFrom, pathTo) {
+  if (pathFrom === pathTo) {
+    return 'You can\' copy file with the same path';
+  }
+
+  try {
+    const data = fs.readFileSync(pathFrom, 'utf-8');
+
+    fs.writeFileSync(pathToCopy, data);
+  } catch (error) {
+    return 'Smt went worng';
+  }
+}
+
+copyFile(pathFromCopy, pathToCopy);
