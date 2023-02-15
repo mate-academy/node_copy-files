e002386e7917d5442467196eb3ba21cd2637018f
Anastasiia Bilska <nastya.bilskaya@gmail.com>
2023-02-16 00:07:47 +0200

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const fs = require('fs');
+
+function copy(originalFilePath, copyFilePath) {
+  if (originalFilePath === copyFilePath) {
+    throw new Error('you are trying to copy data to an output file');
+  }
+
+  try {
+    const content = fs.readFileSync(originalFilePath, 'UTF-8');
+
+    fs.writeFileSync(copyFilePath, content, 'utf-8');
+  } catch (copyError) {
+    return copyError;
+  }
+}
+
+const filesPathes = process.argv.slice(2);
+
+copy(...filesPathes);
