265a914155a2ea33400de002794b6d8ac6b7c736
Anastasiia Bilska <nastya.bilskaya@gmail.com>
2023-02-16 16:58:57 +0200

    solution

--- src/app.js
+++ src/app.js
@@ -1,21 +1,5 @@
 'use strict';
 
-const fs = require('fs');
+const { copy } = require('./copy.js');
 
-function copy(originalFilePath, copyFilePath) {
-  if (originalFilePath === copyFilePath) {
-    throw new Error('You are trying to copy data to an output file');
-  }
-
-  try {
-    const content = fs.readFileSync(originalFilePath, 'UTF-8');
-
-    fs.writeFileSync(copyFilePath, content, 'utf-8');
-  } catch (copyError) {
-    return copyError;
-  }
-}
-
-const filesPathes = process.argv.slice(2);
-
-copy(...filesPathes);
+copy('Enter command name, source file and destination file \n');
