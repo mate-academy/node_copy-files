35e6f67fcc0561206c0876dcacedd687cc051a46
Nurdan Okcu Tanich <nurokcudan@gmail.com>
2023-02-22 18:45:19 +0100

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,17 @@
+/* eslint-disable no-console */
 'use strict';
+
+const readline = require('readline');
+const { copyFile } = require('./copy');
+
+const terminal = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+terminal.question('Please provide a source file to copy  ', (source) => {
+  terminal.question('Please provide a destination  ', destination => {
+    copyFile(source, destination);
+    terminal.close();
+  });
+});
