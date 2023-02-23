2b9d7122ffb3d56f3067d88e5c39f61068f06a60
maxim2310 <m23a09x96@gmail.com>
2023-02-23 22:37:22 +0200

    copyFile is working

--- src/app.js
+++ src/app.js
@@ -1,1 +1,24 @@
+/* eslint-disable no-console */
 'use strict';
+
+const fs = require('fs');
+
+const [argv1, argv2] = process.argv.slice(2);
+
+async function copyFiles(source, destination) {
+  try {
+    const data = await fs.readFileSync(source, 'utf-8');
+
+    if (data) {
+      fs.writeFileSync(destination, data);
+    }
+  } catch (e) {
+    console.warn('something goes wrong(((');
+  }
+}
+
+if (argv1 !== argv2) {
+  copyFiles(argv1, argv2);
+} else {
+  console.warn('source path and destination path must be different');
+}
