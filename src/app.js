bdc6cf4bd4cb5f71262c3bd7a81bdc8698670ba4
Ihor Bondarenko <garryrocksmanjs@gmail.com>
2023-03-06 19:27:00 +0200

    Copy files - completed

--- src/app.js
+++ src/app.js
@@ -1,1 +1,27 @@
 'use strict';
+
+const fs = require('fs');
+const path = require('path');
+
+// for checking how it works, use this command below
+// node src/app.js  cp file.txt file-copy.txt
+
+const copyFiles = () => {
+  const args = process.argv.slice(2);
+
+  const command = args[0];
+  const fromFileDirectory = args[1];
+  const destinationFileDirectory = args[2];
+
+  if (command === 'cp' && fromFileDirectory !== destinationFileDirectory) {
+    const data = fs.readFileSync(path.join(__dirname,
+      fromFileDirectory), 'utf-8');
+
+    fs.writeFileSync(path.join(__dirname,
+      destinationFileDirectory), data);
+  } else {
+    global.console.log('Wrong command - for copying use "cp"');
+  }
+};
+
+copyFiles();
