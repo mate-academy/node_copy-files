902a0ae8329b2f0e2484472be279867b676452cf
Ihor Bondarenko <garryrocksmanjs@gmail.com>
2023-03-08 16:53:38 +0200

    fixes after mentors review

--- src/app.js
+++ src/app.js
@@ -6,10 +6,9 @@ const { copyFiles } = require('./modules/copyFiles');
 // for checking how it works, use command below
 // node src/app.js  cp file.txt file-copy.txt
 
-const args = process.argv.slice(2);
+const [command, from, destination] = process.argv.slice(2);
 
-const command = args[0];
-const fromFileDirectory = path.join(__dirname, args[1]);
-const destinationFileDirectory = path.join(__dirname, args[2]);;
+const fromFileDirectory = path.join(__dirname, from);
+const destinationFileDirectory = path.join(__dirname, destination);
 
 copyFiles(command, fromFileDirectory, destinationFileDirectory);
--- src/modules/copyFiles.js
+++ src/modules/copyFiles.js
@@ -4,19 +4,12 @@ const fs = require('fs');
 
 const copyFiles = (command, fromFileDirectory, destinationFileDirectory) => {
   if (command === 'cp' && fromFileDirectory !== destinationFileDirectory) {
-    let data = '';
-
     try {
-      data = fs.readFileSync(fromFileDirectory, 'utf-8');
-    } catch (error) {
-      global.console.log('Unable to read file, no such file of directory');
-    }
+      const data = fs.readFileSync(fromFileDirectory, 'utf-8');
 
-    try {
       fs.writeFileSync(destinationFileDirectory, data);
-      global.console.log('File successfully copied');
     } catch (error) {
-      global.console.log('Unable to write file');
+      global.console.log(`Something went wrong! Error code - ${error.message}`);
     }
   } else {
     global.console.log('Wrong command - for copying use "cp"');
