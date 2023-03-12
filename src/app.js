b275b0055a06f737a0f5f3ea799d90d9197273ce
Eugene Khablenko <eugene.khablenko@gmail.com>
2023-03-13 01:05:28 +0200

    develop

--- src/app.js
+++ src/app.js
@@ -1,1 +1,41 @@
-'use strict';
+"use strict";
+
+const fs = require("fs");
+const terminal = require('./modules/terminal');
+const copyFile = require('./modules/copy');
+console.log(copyFile);
+
+const prompt = () => {
+  console.log("Let's copy a file!");
+  terminal.question(
+    "Which file do you want to copy? Enter full path: ",
+    (fileName) => {
+      console.log(fileName);
+
+      if (!fs.existsSync(fileName)) {
+        console.log("This file does not exist in this folder, try again");
+        prompt();
+      } else {
+        terminal.question(
+          "Where do you want to copy the file? Enter full path: ",
+          (destPath) => {
+            if (fileName === destPath) {
+              terminal.question(
+                "Cannot copy file to the same folder, enter a different path: ",
+                (destPath) => {
+                  copyFile(sourcePath, destPath);
+                  terminal.close();
+                }
+              );
+            } else {
+              copyFile(fileName, destPath);
+              terminal.close();
+            }
+          }
+        );
+      }
+    }
+  );
+};
+
+prompt();
