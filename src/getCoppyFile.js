b5b678e1b48d4ad38ff3fe200fb3f85dc3719180
Olha Yevstifieieva <hob.bilbo@gmail.com>
2023-03-12 12:28:39 +0200

    Refactoring code after student review

--- src/getCoppyFile.js
+++ src/getCoppyFile.js
@@ -1,33 +1,25 @@
 /* eslint-disable no-console */
 'use strict';
 
-const fs = require('fs');
+const fs = require('fs/promises');
 
 const path = require('path');
 
-function getCopyFile(source, destination) {
+const getCopyFile = async(source, destination) => {
   const sourcePath = path.resolve(source);
   const destPath = path.resolve(destination);
 
   if (sourcePath === destPath) {
-    console.log('Source and destination are the same.');
-
-    return;
-  }
-
-  if (!fs.existsSync(sourcePath)) {
-    console.log(`Source file '${sourcePath}' does not exist.`);
-
     return;
   }
 
   try {
-    fs.copyFileSync(sourcePath, destPath);
+    await fs.copyFileSync(sourcePath, destPath);
     console.log(`'${sourcePath}' copied to '${destPath}' successfully.`);
   } catch (err) {
     console.log(`Error copying '${sourcePath}' to '${destPath}':
     ${err.message}`);
   }
-}
+};
 
 module.exports = { getCopyFile };
