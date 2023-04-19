0a5e78c2ff93983f3284f3212ac9225b73db218c
Stanislav Korchevskyi <stanislav.codes@gmail.com>
2023-04-19 23:26:24 +0300

    more checks and error handling

--- src/copy.js
+++ src/copy.js
@@ -4,15 +4,35 @@
 const fs = require('fs');
 
 const copyFile = (fromPath, toPath) => {
+  if (!fromPath || !toPath) {
+    console.error(
+      'Please make sure both paths is defined!',
+    );
+
+    return;
+  }
+
+  if (fromPath === toPath) {
+    console.error('Cannot copy to the same location!');
+
+    return;
+  }
+
   if (!fs.existsSync(fromPath)) {
     console.error('Source file does not exist!');
 
     return;
   }
 
-  fs.copyFileSync(fromPath, toPath);
+  try {
+    fs.copyFileSync(fromPath, toPath);
 
-  console.log(`Successfully copied from "${fromPath}" to "${toPath}"`);
+    console.log(`Successfully copied from "${fromPath}" to "${toPath}"`);
+  } catch (err) {
+    console.error(
+      `An error occurred while copying from "${fromPath}" to "${toPath}"`,
+    );
+  }
 };
 
 module.exports = { copyFile };
