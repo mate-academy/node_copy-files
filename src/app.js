08e6aa059709f6fd8fb2fa7337be4c8bff4125db
Deniskakaka <krollik1000000@gmail.com>
2023-04-26 22:49:52 +0300

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,27 @@
 'use strict';
+
+const fs = require('fs');
+const path = require('path');
+
+function copyFile(fileName, result) {
+  const sourcePath = path.resolve(fileName);
+  const targetPath = path.resolve(result);
+
+  fs.readFile(sourcePath, (err, data) => {
+    if (err) {
+      // eslint-disable-next-line no-console
+      console.log('this file not found');
+    }
+
+    fs.writeFile(targetPath, data, (error) => {
+      if (error) {
+        // eslint-disable-next-line no-console
+        console.log('some thing was wrong');
+      }
+    });
+  });
+};
+
+const [, , source, target] = process.argv;
+
+copyFile(source, target);
