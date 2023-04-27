700924486988314d2eaf0c6c084eb0970177eba8
Valeriia Yarzhemska <lera.strikalo@icloud.com>
2023-04-27 18:45:20 +0300

    Solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,36 @@
+/* eslint-disable no-console */
 'use strict';
+
+const fs = require('fs');
+const path = require('path');
+
+function copyFileToDestination() {
+  const [src, dest] = process.argv.slice(2);
+
+  if (process.argv.length < 4) {
+    console.log('Please enter file name and destination to copy');
+
+    return;
+  }
+
+  if (src === dest) {
+    console.log('You are trying to copy file to the same destination');
+
+    return;
+  }
+
+  const source = path.join(__dirname, src);
+  const destination = path.join(__dirname, dest);
+
+  fs.copyFile(source, destination, (error) => {
+    if (error) {
+      console.log(error);
+
+      return;
+    }
+
+    console.log('File was coppied');
+  });
+}
+
+copyFileToDestination();
