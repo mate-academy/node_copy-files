9d3abab62e904865e44275e9c8e6e1b80e11012c
Serhii Zelinskyi <zelinskyi.serh@gmail.com>
2023-04-27 14:14:47 +0300

    fixed solution

--- src/app.js
+++ src/app.js
@@ -7,11 +7,19 @@ function copyFile() {
   const terminalParameters = process.argv.slice(2);
   const copyFromFile = terminalParameters[0];
   const copyToFile = terminalParameters[1];
+
+  if (!copyFromFile || !copyToFile) {
+    global.console.log(
+      'You need to indicate which file you want to make a copy from',
+      'For example: file.txt file-copy.txt'
+    );
+  }
+
   const pathToCopyFromFile = path.join(__dirname, copyFromFile);
   const pathToCopyToFile = path.join(__dirname, copyToFile);
 
   if (pathToCopyFromFile === pathToCopyToFile) {
-    global.console.log('You try copy file to the same place');
+    global.console.log('You are trying to copy file to itself');
 
     return;
   }
