9033389f0471265fa98828ef2e4d73698b3da22c
kirill-vypirovskyi <kirill.vypirovsky@gmail.com>
2023-04-28 10:49:07 +0300

    mistakes fixed

--- src/app.js
+++ src/app.js
@@ -6,13 +6,19 @@ const path = require('path');
 const { terminal } = require('./terminal');
 
 function copyFile(source, destination) {
-  const sourcePath = path.resolve(source);
-  const destPath = path.resolve(destination);
+  const sourcePath = path.resolve(__dirname, source);
+  const destPath = path.resolve(__dirname, destination);
+
+  if (!fs.existsSync(sourcePath)) {
+    console.log('File does not exist');
+    ask();
+
+    return;
+  }
 
   if (sourcePath === destPath) {
     console.log('Source and destination are the same. Nothing to do.');
-
-    return;
+    terminal.close();
   }
 
   const readStream = fs.createReadStream(sourcePath);
@@ -23,18 +29,24 @@ function copyFile(source, destination) {
   writeStream.on('close', () => {
     console.log(`Successfully copied ${sourcePath} to ${destPath}.`);
     terminal.close();
+
+    ask();
   });
 }
 
-terminal.question('write paths: from... to... ', (answer) => {
-  const paths = answer.split(' ');
+const ask = () => {
+  terminal.question('write paths: from... to... ', (answer) => {
+    const paths = answer.split(' ');
 
-  if (paths.length !== 2) {
-    console.log('impossible path');
-    terminal.close();
+    if (paths.length !== 2) {
+      console.log('impossible path');
+      ask();
 
-    return;
-  }
+      return;
+    }
 
-  copyFile(...paths);
-});
+    copyFile(...paths);
+  });
+};
+
+ask();
