2fbb3643a1c4d33a7c53d75fe756a9e8f761714f
Sergey Lipikhin <football3@ukr.net>
2023-04-28 20:09:22 +0300

    Finish solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const fs = require('fs');
+const { resolve } = require('path');
+
+const [,, source, destination] = process.argv;
+
+try {
+  if (resolve(source) === resolve(destination)) {
+    throw new Error('Cannot copy file to itself');
+  }
+
+  const fileContent = fs.readFileSync(source);
+
+  fs.writeFileSync(destination, fileContent);
+} catch (exception) {
+  process.stdout.write(exception.message);
+}
