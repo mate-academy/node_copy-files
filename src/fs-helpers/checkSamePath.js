3bf4108905e01dc704870ae871e5ba854d16ad89
Dovhun Artem <dovhun3d@gmail.com>
2023-04-28 12:17:59 +0300

    Fix logic

--- src/fs-helpers/checkSamePath.js
+++ src/fs-helpers/checkSamePath.js
@@ -3,6 +3,14 @@
 const path = require('path');
 
 const checkSamePath = (source, destination) => {
+  if (!source || !destination) {
+    global.console.log(
+      'The source or destination does not exist',
+    );
+
+    process.exit();
+  }
+
   if (path.resolve(source) === path.resolve(destination)) {
     global.console.log(
       'The source and destination are the same.',
