59727a246fcb39fae55825401328538a529fb0f0
Valeriia Yarzhemska <lera.strikalo@icloud.com>
2023-04-29 14:35:31 +0300

    fixed destinations issues

--- src/app.js
+++ src/app.js
@@ -2,10 +2,9 @@
 'use strict';
 
 const fs = require('fs');
-const path = require('path');
 
 function copyFileToDestination() {
-  const [src, dest] = process.argv.slice(2);
+  const [source, dest] = process.argv.slice(2);
 
   if (process.argv.length < 4) {
     console.log('Please enter file name and destination to copy');
@@ -13,16 +12,13 @@ function copyFileToDestination() {
     return;
   }
 
-  if (src === dest) {
+  if (source === dest) {
     console.log('You are trying to copy file to the same destination');
 
     return;
   }
 
-  const source = path.join(__dirname, src);
-  const destination = path.join(__dirname, dest);
-
-  fs.copyFile(source, destination, (error) => {
+  fs.copyFile(source, dest, (error) => {
     if (error) {
       console.log(error);
 
