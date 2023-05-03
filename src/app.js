ca5997288653cefa7006794fedfa9591e1501642
Misha Ivchenko <ivchenkomykhail@meta.ua>
2023-05-03 15:01:43 +0300

    Solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const fs = require('fs-extra');
+
+const command = process.argv[2];
+const originalFile = process.argv[3];
+const copyFile = process.argv[4];
+
+if (command === 'cp' && originalFile !== copyFile) {
+  fs.copy(originalFile, copyFile, (err) => {
+    if (err) {
+      return err;
+    }
+
+    return 'success';
+  });
+}
