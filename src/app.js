0b3aacaa4bb656a44aac3f1633f67d32ee305c44
Sergey Lipikhin <football3@ukr.net>
2023-05-04 07:05:59 +0300

    Use built-in copy function

--- src/app.js
+++ src/app.js
@@ -1,18 +1,11 @@
 'use strict';
 
 const fs = require('fs');
-const { resolve } = require('path');
 
 const [,, source, destination] = process.argv;
 
 try {
-  if (resolve(source) === resolve(destination)) {
-    throw new Error('Cannot copy file to itself');
-  }
-
-  const fileContent = fs.readFileSync(source);
-
-  fs.writeFileSync(destination, fileContent);
+  fs.copyFileSync(source, destination, fs.constants.COPYFILE_EXCL);
 } catch (exception) {
   process.stdout.write(exception.message);
 }
