65a9aaf2f8f44bf57bdd6f363fb37e73a49d1a17
Olga Bordiug <obordiug@gmail.com>
2023-05-17 12:14:02 +0300

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,24 @@
+/* eslint-disable no-console */
 'use strict';
+
+const fs = require('fs');
+
+const copyFile = (sourcePath, destPath) => {
+  if (sourcePath === destPath) {
+    console.log('source and dest paths are the same');
+
+    return;
+  }
+
+  fs.copyFileSync(sourcePath, destPath, (error) => {
+    if (error) {
+      console.error(`Error while copying file: ${error}`);
+    } else {
+      console.log('File copied successfully.');
+    }
+  });
+};
+
+const [source, dest] = process.argv.slice(2);
+
+copyFile(source, dest);
