87833be8202b2dcc880cf7e3f1935f4a14b8a805
Daria <hamdsad8@gmail.com>
2023-05-26 10:49:37 +0300

    created a function which copies files

--- src/app.js
+++ src/app.js
@@ -1,1 +1,25 @@
 'use strict';
+
+const fs = require('fs');
+
+const copyFile = (copyFrom, copyTo) => {
+  if (copyFrom === copyTo) {
+    throw new Error('The pathes are the same');
+  }
+
+  if (!copyFrom || !copyTo) {
+    throw new Error('You should enter 2 valid pathes');
+  }
+
+  fs.copyFileSync(copyFrom, copyTo, (err) => {
+    if (err) {
+      throw new Error('Something went wrong');
+    }
+
+    return 'The file was copied successfully';
+  });
+};
+
+const [source, destination] = process.argv.slice(2);
+
+copyFile(source, destination);
