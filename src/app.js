47bf2791c2907be0d9223014239b857323cf026e
Aleksandr <alexnovaklawyer@gmail.com>
2023-05-30 17:55:56 +0300

    features added

--- src/app.js
+++ src/app.js
@@ -1,11 +1,22 @@
 'use strict';
 
 const fs = require('fs');
+const path = require('path');
 
 const [fileName, newFileName] = process.argv.slice(2);
 
 function copy() {
-  fs.copyFileSync(fileName, newFileName);
+  if (fileName === newFileName) {
+    // eslint-disable-next-line no-console
+    console.log('Name is already exists');
+
+    return;
+  }
+
+  const currentPath = path.resolve();
+
+  // eslint-disable-next-line max-len
+  fs.copyFileSync(`${currentPath}/src/${fileName}`, `${currentPath}/src/${newFileName}`);
 }
 
 copy();
