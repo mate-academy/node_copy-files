8c848c5b729093383828bb8749ec996b383bd070
Mykyta Holtsov <nikitaholtsov@gmail.com>
2023-05-30 16:17:15 +0300

    node copy files solved

--- src/app.js
+++ src/app.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const fs = require('fs');
+const datas = process.argv.slice(2);
+
+if (datas[0] === 'cp') {
+  copyFile(datas[1], datas[2]);
+}
+
+function copyFile(file, copy) {
+  try {
+    const textFromFile = fs.readFileSync(`${file}`, 'utf8');
+
+    fs.writeFileSync(`${copy}`, textFromFile);
+  } catch (error) {
+    throw new Error(error);
+  }
+}
