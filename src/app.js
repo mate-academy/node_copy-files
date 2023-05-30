a64c6adb668a11122e0eab638a30c143d8bdb36d
BondarenkoDmitriy <bondarenko.dima.kiev@gmail.com>
2023-05-30 18:29:16 +0300

    works

--- src/app.js
+++ src/app.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const fs = require('fs');
+
+const copyFile = () => {
+  try {
+    if (process.argv[2] === 'cp') {
+      const [file, copy] = process.argv.slice(3);
+
+      fs.copyFileSync(file, copy);
+    }
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+copyFile();
