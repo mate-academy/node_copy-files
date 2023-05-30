ebac74f5f7d9ba3dd9340dd08852bd9156aa379b
Valery Sazonov <vksazonov@gmail.com>
2023-05-30 18:56:31 +0300

    Solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const fs = require('fs');
+
+const copyFunc = () => {
+  try {
+    if (process.argv[2] === 'cp') {
+      const [file, copy] = process.argv.slice(3);
+
+      fs.copyFileSync(file, copy);
+    }
+  } catch (err) {
+    throw new Error('Error: cp: no such file or directory');
+  }
+};
+
+copyFunc();
