e657c05cc237b5fc76fa73d3cfcc3e4cefe28c7b
BondarenkoDmitriy <bondarenko.dima.kiev@gmail.com>
2023-05-31 13:49:29 +0300

    fixed all comments

--- src/app.js
+++ src/app.js
@@ -1,17 +1,32 @@
+/* eslint-disable no-console */
 'use strict';
 
 const fs = require('fs');
 
-const copyFile = () => {
-  try {
-    if (process.argv[2] === 'cp') {
-      const [file, copy] = process.argv.slice(3);
+const copyFile = (file, copy) => {
+  if (file === copy) {
+    console.log(
+      'Source file and destination file are the same. No file copied.'
+    );
 
-      fs.copyFileSync(file, copy);
-    }
+    return;
+  }
+
+  try {
+    fs.copyFileSync(file, copy);
+    console.log('File copied successfully.');
   } catch (error) {
-    throw new Error(error);
+    console.log('An error occurred while copying the file:', error.message);
   }
 };
 
-copyFile();
+const main = () => {
+  if (process.argv[2] === 'cp') {
+    // eslint-disable-next-line no-unused-vars
+    const [_, __, file, copy] = process.argv;
+
+    copyFile(file, copy);
+  }
+};
+
+main();
