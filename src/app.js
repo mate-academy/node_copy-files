134e76172b1e87c395503b2f0f775396d3bbe5b7
Mykyta Holtsov <nikitaholtsov@gmail.com>
2023-05-31 17:11:40 +0300

    small fixes

--- src/app.js
+++ src/app.js
@@ -1,18 +1,23 @@
+/* eslint-disable no-console */
 'use strict';
 
 const fs = require('fs');
-const datas = process.argv.slice(2);
+const args = process.argv.slice(2);
 
-if (datas[0] === 'cp') {
-  copyFile(datas[1], datas[2]);
+if (args[0] === 'cp' && args[1] !== args[2]) {
+  copyFile(args[1], args[2]);
 }
 
 function copyFile(file, copy) {
+  if (!file || !copy) {
+    return 'Write file name which need copy and name copy file.';
+  }
+
   try {
     const textFromFile = fs.readFileSync(`${file}`, 'utf8');
 
     fs.writeFileSync(`${copy}`, textFromFile);
   } catch (error) {
-    throw new Error(error);
+    return error;
   }
 }
