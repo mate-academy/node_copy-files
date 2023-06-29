6569ecde6870e8dd7c3d981d44741d1851feb727
Illia Kyrychenko <i.kirichenko160804@gmail.com>
2023-06-29 23:38:10 +0300

    Async file work and try catch was added

--- src/app.js
+++ src/app.js
@@ -6,12 +6,26 @@ const firstFilePath = path.join(__dirname, '/file.txt');
 const secondFilePath = path.join(__dirname, '/file-copy.txt');
 
 const copyFiles = (file, fileToCopy) => {
-  if (file !== fileToCopy) {
-    const firstFileData = fs.readFileSync(file, 'utf-8');
+  try {
+    if (file === fileToCopy) {
+      throw new Error('Do not copy data to the same file');
+    }
 
-    fs.writeFileSync(fileToCopy, firstFileData);
-  } else {
-    return 'Trying to copy to the same location';
+    const firstFileData = fs.readFileSync(file, 'utf-8', (err, data) => {
+      if (err) {
+        throw new Error('Some error occurred while reading a file');
+      }
+
+      return data;
+    });
+
+    fs.writeFileSync(fileToCopy, firstFileData, (err) => {
+      if (err) {
+        throw new Error('Some error occurred while writing to a file');
+      }
+    });
+  } catch (error) {
+    // Some error handling
   }
 };
 
