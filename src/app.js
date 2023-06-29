15bab392fd3822af17faf2a5184299d74fb8dd3a
Illia Kyrychenko <i.kirichenko160804@gmail.com>
2023-06-29 11:45:26 +0300

    Solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const fs = require('fs');
+const path = require('path');
+const firstFilePath = path.join(__dirname, '/file.txt');
+const secondFilePath = path.join(__dirname, '/file-copy.txt');
+
+const copyFiles = (file, fileToCopy) => {
+  if (file !== fileToCopy) {
+    const firstFileData = fs.readFileSync(file, 'utf-8');
+
+    fs.writeFileSync(fileToCopy, firstFileData);
+  } else {
+    return 'Trying to copy to the same location';
+  }
+};
+
+copyFiles(firstFilePath, secondFilePath);
