7f247313e1b1546903cf3c00f80df70bfbcb6b73
OleksandrV <oleksandr.v@vevidi.com>
2023-07-18 10:25:43 +0300

    for review

--- src/app.js
+++ src/app.js
@@ -1,1 +1,24 @@
 'use strict';
+
+const fs = require('fs');
+
+const cp = () => {
+  const [prevPath, newPath] = process.argv.slice(2);
+
+  if (prevPath === newPath) {
+    return;
+  }
+
+  try {
+    const data = fs.readFileSync(prevPath, 'utf-8');
+
+    fs.writeFileSync(newPath, data);
+  } catch (e) {
+    // eslint-disable-next-line
+    console.log('something went wrong', e);
+  }
+};
+
+cp();
+
+module.exports = { cp };
