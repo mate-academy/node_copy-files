c1d940df6fdb7c168aa50a1c63a805bab1dea8dd
Paweł Smoliński <ski1993@o2.pl>
2023-07-20 19:42:54 +0200

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const { copyFile } = require('fs');
+const { dirname } = require('path');
+
+const [ , , copyFrom, copyTo ] = process.argv;
+
+(() => {
+  const pathFrom = dirname(copyFrom);
+  const pathTo = dirname(copyTo);
+
+  if (pathFrom !== pathTo) {
+    copyFile(copyFrom, copyTo, (error) => {
+      if (error) {
+        throw new Error('Failed to copy');
+      }
+    });
+  }
+})();
