fab5059256987f5938fc6619814e5ca04f88994e
Vadym Prydatok <vadymprydatok@gmail.com>
2023-07-25 19:46:29 +0300

    solved

--- src/app.js
+++ src/app.js
@@ -1,1 +1,32 @@
+/* eslint-disable no-console */
 'use strict';
+
+const fs = require('fs');
+
+const createCopy = (pathToFile, pathToCopyFile) => {
+  let oldFile;
+
+  if (pathToFile === pathToCopyFile) {
+    return;
+  }
+
+  try {
+    const data = fs.readFileSync(pathToFile, 'utf8');
+
+    oldFile = data;
+  } catch (error) {
+    throw new Error('Wrong path to file');
+  }
+
+  try {
+    fs.writeFileSync(pathToCopyFile, oldFile);
+  } catch (error) {
+    throw new Error('Wrong path to copy file');
+  }
+
+  console.log('Copy was created');
+};
+
+const [path, pathForCopy] = process.argv.slice(2);
+
+createCopy(path, pathForCopy);
