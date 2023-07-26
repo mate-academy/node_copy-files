8a98bc2750f0069002481100a7d3a3615be5e1f3
GGsmou <svyatoslav.golovin.2005@gmail.com>
2023-07-26 18:53:40 +0300

    add try catch

--- src/copy.js
+++ src/copy.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 'use strict';
 
 const fs = require('fs');
@@ -7,7 +8,13 @@ const copy = (src, dest) => {
     return;
   }
 
-  fs.writeFileSync(dest, fs.readFileSync(src, 'utf8'));
+  try {
+    const fileContent = fs.readFileSync(src, 'utf8');
+
+    fs.writeFileSync(dest, fileContent);
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 module.exports = { copy };
