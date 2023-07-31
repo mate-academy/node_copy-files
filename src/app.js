5b5b4091308532d49cc5d616b7df32edeaa993d3
Oleksii Lavka <lavka.alexey37gmail.com>
2023-07-31 14:49:39 +0300

    add task solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const path = require('path');
+const { copyFile } = require('./modules/copyFile.js');
+
+const startCopy = () => {
+  const [source, dest] = process.argv.slice(2);
+
+  if (!source || !dest) {
+    throw new Error(
+      'You should pass the way to the source file and the way to the dest file.'
+    );
+  }
+
+  const sourcePath = path.join(__dirname, source);
+  const destPath = path.join(__dirname, dest);
+
+  copyFile(sourcePath, destPath);
+};
+
+startCopy();
