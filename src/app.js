b6ddbd21ec26f5d70020b7eeec93eea98bda62e6
Mykhailo Kostov <kostov.mykhailo@gmail.com>
2023-08-03 14:35:03 +0300

    Solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,24 @@
 'use strict';
+
+const terminal = require('./modules/terminal');
+const fs = require('fs');
+
+function copyFile() {
+  terminal.question('Enter your command: ', (key) => {
+    const [command, firstFile, secondFile] = key.split(' ');
+
+    if (command === 'cp' && !fs.existsSync(secondFile)) {
+      fs.copyFile(firstFile, secondFile, (err) => {
+        if (err) {
+          throw new Error('Error!');
+        }
+
+        // eslint-disable-next-line no-console
+        console.log(`${firstFile} was copied as ${secondFile}`);
+      });
+    }
+    terminal.close();
+  });
+};
+
+copyFile();
