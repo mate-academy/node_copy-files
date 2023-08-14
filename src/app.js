08f346e9ffd12016174c246f2943ef39afa71ed3
Ilia <koleshnik160799@gmail.com>
2023-08-14 20:59:53 +0300

    Done

--- src/app.js
+++ src/app.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const fs = require('fs');
+
+function funcRead() {
+  const terminal = process.argv.slice(2);
+  const filePath = './src/' + terminal[0];
+  const newFilePath = './src/' + terminal[1];
+  const isFile = fs.existsSync(filePath);
+
+  if (isFile) {
+    const fileInfo = fs.readFileSync(filePath, 'utf8');
+
+    fs.writeFile(newFilePath, fileInfo, (err, data) => {
+      if (err) {
+
+      }
+    });
+  }
+}
+
+funcRead();
