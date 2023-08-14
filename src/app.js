81c7cf746d4be1e81bf4a8587a18e7229941063b
Dima Cherednyk <dicherednyk@gmail.com>
2023-08-14 20:31:16 +0300

    Solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,10 @@
+/* eslint-disable no-console */
 'use strict';
+
+const fs = require('fs');
+
+fs.cp('./src/1.txt', './src/1 copy.txt', (err) => {
+  if (err) {
+    console.log('Error Found:', err);
+  }
+});
