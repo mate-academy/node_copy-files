e3e025fa873d3de37ef869e533dff08a9c51ac5b
Ivan Bilotserkivskiy <ivanbilotserkivskiy56@gmail.com>
2023-08-15 18:40:30 +0300

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const fs = require('fs');
+const path = require('path');
+
+function coppyFile(fileToCopy, newFile) {
+  if (fileToCopy === newFile) {
+    return;
+  }
+
+  const dataToWrite = fs.readFileSync(fileToCopy, { encoding: 'utf-8' });
+
+  fs.writeFileSync(newFile, dataToWrite);
+}
+
+const fromFile = path.join(__dirname, process.argv[2]);
+const toFile = path.join(__dirname, process.argv[3]);
+
+coppyFile(fromFile, toFile);
