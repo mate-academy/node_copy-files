97740a3207c32eea4af6df03ec919c92a08221c9
IlonaBorkovska <borkovska.io@gmail.com>
2023-08-16 01:13:01 +0300

    add task solution

--- src/app.js
+++ src/app.js
@@ -2,16 +2,31 @@
 
 const terminal = require('./modules/terminal');
 const fs = require('fs');
+const path = require('path');
 
 function copyFile() {
   terminal.question('Enter your command: ', (key) => {
     const [command, firstFile, secondFile] = key.trim().split(' ');
+    const firstFilePath = path.join(__dirname, firstFile);
+    const secondFilePath = path.join(__dirname, secondFile);
 
     if (command === 'cp'
-      && fs.existsSync(firstFile)
-      && !fs.existsSync(secondFile)
-      ) {
-      fs.copyFile(firstFile, secondFile, (err) => {
+      && fs.existsSync(firstFilePath)
+      && !fs.existsSync(secondFilePath)
+    ) {
+      const content = fs.readFileSync(
+        firstFilePath,
+        'utf8',
+        (err, data) => {
+          if (err) {
+            throw new Error(err);
+          }
+
+          return data;
+        },
+      );
+
+      fs.writeFileSync(secondFilePath, content, 'utf8', (err) => {
         if (err) {
           throw new Error(err);
         }
@@ -20,6 +35,7 @@ function copyFile() {
         console.log(`${firstFile} was copied as ${secondFile}`);
       });
     }
+
     terminal.close();
   });
 };
