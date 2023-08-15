660581254125faa52d22f46f67d80789e9dbf35d
Ilia <koleshnik160799@gmail.com>
2023-08-15 16:49:00 +0300

    fix code

--- src/app.js
+++ src/app.js
@@ -1,22 +1,39 @@
 'use strict';
 
 const fs = require('fs');
+const path = require('path');
 
-function funcRead() {
-  const terminal = process.argv.slice(2);
-  const filePath = './src/' + terminal[0];
-  const newFilePath = './src/' + terminal[1];
-  const isFile = fs.existsSync(filePath);
+const terminal = process.argv.slice(2);
 
-  if (isFile) {
-    const fileInfo = fs.readFileSync(filePath, 'utf8');
+function copyFile(donerPath, destinationPath) {
+  if (donerPath === destinationPath) {
+    process.stdout.write('You cannot copy the same file');
 
-    fs.writeFile(newFilePath, fileInfo, (err, data) => {
-      if (err) {
+    return;
+  }
 
+  fs.readFile(path.resolve(donerPath), (err, data) => {
+    if (err) {
+      process.stdout.write('Error reading file');
+
+      return;
+    }
+
+    fs.writeFile(path.resolve(destinationPath), data, (error) => {
+      if (error) {
+        process.stdout.write('Error writing file');
       }
     });
-  }
+
+    process.stdout.write('File copied');
+  });
 }
 
-funcRead();
+if (terminal.length < 2) {
+  process.stdout.write('Need any params');
+} else {
+  const filePath = path.join(__dirname, terminal[0]);
+  const newFilePath = path.join(__dirname, terminal[1]);
+
+  copyFile(filePath, newFilePath);
+}
