78e53e39de15e04511ed41fa56f6abade8c183cb
Oleh Khalanskyi <okhalanskyi@gmail.com>
2023-08-16 18:07:59 +0300

    fix: fixed with stream

--- src/copy.service.js
+++ src/copy.service.js
@@ -1,12 +1,22 @@
 'use strict';
 
 const fs = require('fs');
+// const stream = require('stream/promises');
 
 const copy = (from, to) => {
   try {
-    const data = fs.readFileSync(from);
+    const readStream = fs.createReadStream(from);
 
-    fs.writeFileSync(to, data);
+    const writeStream = fs.createWriteStream(to);
+
+    const handleError = () => {
+      readStream.destroy();
+      writeStream.end('finish');
+    };
+
+    readStream
+      .pipe(writeStream)
+      .on('error', handleError);
   } catch (e) {
     throw new Error(e);
   }
