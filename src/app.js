74a2982c28d5b13879a00d018cdaa4e301afa971
Ivan Bilotserkivskiy <ivanbilotserkivskiy56@gmail.com>
2023-08-16 15:04:17 +0300

    remove dirname and encoding

--- src/app.js
+++ src/app.js
@@ -1,19 +1,18 @@
 'use strict';
 
 const fs = require('fs');
-const path = require('path');
 
 function coppyFile(fileToCopy, newFile) {
   if (fileToCopy === newFile) {
     return;
   }
 
-  const dataToWrite = fs.readFileSync(fileToCopy, { encoding: 'utf-8' });
+  const dataToWrite = fs.readFileSync(fileToCopy);
 
   fs.writeFileSync(newFile, dataToWrite);
 }
 
-const fromFile = path.join(__dirname, process.argv[2]);
-const toFile = path.join(__dirname, process.argv[3]);
+const fromFile = process.argv[2];
+const toFile = process.argv[3];
 
 coppyFile(fromFile, toFile);
