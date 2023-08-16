b060901445bd5306f7593767c16b1d2d391c811a
Dima Cherednyk <dicherednyk@gmail.com>
2023-08-16 21:25:17 +0300

    Solution

--- src/app.js
+++ src/app.js
@@ -2,8 +2,24 @@
 'use strict';
 
 const fs = require('fs');
+const { faker } = require('@faker-js/faker');
 
-fs.cp('./src/1.txt', './src/1 copy.txt', (err) => {
+const content = faker.lorem.word();
+const fileName = faker.lorem.word();
+const firstPath = `./src/${fileName}.txt`;
+const secondPath = `./src/${fileName} copy.txt`;
+
+fs.writeFile(firstPath, content, (error) => {
+  if (error) {
+    console.error(error);
+  }
+});
+
+fs.cp(firstPath, secondPath, (err) => {
+  if (firstPath === secondPath) {
+    return;
+  }
+
   if (err) {
     console.log('Error Found:', err);
   }
