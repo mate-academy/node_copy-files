718a717c2b1b420a79fcd35636d45664c9240332
Iryna <irinabolbat98@gmail.com>
2023-08-17 14:10:15 +0300

    add task solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,33 @@
+/* eslint-disable no-console */
 'use strict';
+
+const fs = require('fs');
+
+const copyFile = () => {
+  const args = process.argv.slice(2);
+
+  if (args.length !== 2) {
+    console.log('No files for copy');
+
+    return;
+  }
+
+  const filePath = args[0];
+  const copiedFilePath = args[1];
+
+  if (filePath === copiedFilePath) {
+    console.log('Paths of files are the same');
+
+    return;
+  }
+
+  try {
+    const data = fs.readFileSync(filePath);
+
+    fs.writeFileSync(copiedFilePath, data);
+  } catch (err) {
+    console.error(err);
+  };
+};
+
+copyFile();
