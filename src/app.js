f4493ee095d510238166b043ab955dd777eb8ef9
Iryna <irinabolbat98@gmail.com>
2023-08-19 21:05:39 +0300

    fix code

--- src/app.js
+++ src/app.js
@@ -6,6 +6,12 @@ const fs = require('fs');
 const copyFile = () => {
   const args = process.argv.slice(2);
 
+  if (args.length === 1) {
+    console.error('No destination path');
+
+    return;
+  }
+
   if (args.length !== 2) {
     console.log('No files for copy');
 
@@ -22,9 +28,11 @@ const copyFile = () => {
   }
 
   try {
-    const data = fs.copyFileSync(filePath);
-
-    fs.writeFileSync(copiedFilePath, data);
+    if (fs.existsSync(filePath)) {
+      fs.copyFileSync(filePath, copiedFilePath);
+    } else {
+      console.log('Origin path does not exist');
+    }
   } catch (err) {
     console.error(err);
   };
