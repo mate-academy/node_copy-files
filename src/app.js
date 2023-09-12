535b8beebe420272d16787bae4b819ba9590d9c2
Rodion Pushkarskiy <pushkarskiyrodion@gmail.com>
2023-09-12 22:53:26 +0300

    the task was done

--- src/app.js
+++ src/app.js
@@ -1,1 +1,17 @@
+/* eslint-disable no-console */
 'use strict';
+
+const fs = require('fs');
+const [ src, dest ] = process.argv.slice(2);
+
+fs.copyFile(src, dest, (err) => {
+  if (src === dest) {
+    throw new Error('You are trying to copy to the same location');
+  }
+
+  try {
+    console.log(`The file was copied from ${src} to ${dest}`);
+  } catch (error) {
+    console.log(err);
+  }
+});
