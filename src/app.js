c512b8365e82cfd1f2ba0b39996e52afa9747723
Izzet Yusufov <17izzet@gmail.com12345Hello>
2023-09-19 22:24:10 +0200

    solution

--- src/app.js
+++ src/app.js
@@ -4,81 +4,24 @@
 const fs = require('fs');
 const path = require('path');
 
-const [fileToCopy, targetPath] = process.argv.slice(2);
-const startDir = './src';
+const [sourceFile, targetFile] = process.argv.slice(2);
 
-if (!fileToCopy || !targetPath) {
+const dirPath = path.dirname(targetFile);
+const baseNameFile = path.basename(sourceFile);
+const targetFilePath = path.join(dirPath, baseNameFile);
+const filesInside = fs.readdirSync(dirPath);
+
+if (filesInside.includes(baseNameFile)) {
   return console.log(
-    'Unable to proceed. Please ensure that'
-    + 'you specified 2 paths. e.g node src/app.js file1 file2'
+    `Error, couldn't copy the file.`
+    + `${baseNameFile} already exist in this directory`
   );
 }
 
-const findTheFile = (dir) => {
-  const allFiles = [];
-
-  const filesInDir = fs.readdirSync(dir);
-
-  filesInDir.forEach(currFile => {
-    const pathName = `${dir}${path.sep}${currFile}`;
-
-    if (!fs.lstatSync(pathName).isDirectory()) {
-      if (currFile === fileToCopy || currFile === targetPath) {
-        allFiles.push(pathName);
-      };
-    } else {
-      const insideDirFile = findTheFile(pathName);
-
-      allFiles.push(...insideDirFile);
-    }
-  });
-
-  return allFiles;
-};
-
-const foundFiles = findTheFile(startDir);
-
-const checkTheSameFile = [...new Set(foundFiles.map(f => path.basename(f)))];
-
-if (checkTheSameFile.length < 2) {
-  return console.log(
-    'In target directory already exist the file with such name!'
-  );
-};
-
-const [file, targetFolder] = foundFiles;
-
-if (foundFiles.length < 2) {
-  if (foundFiles.length === 0) {
-    return console.log(
-      `Unable to proceed. File: ${fileToCopy}`
-      + `and ${targetPath} doesn't exist in derictory!`
-    );
-  }
-
-  if (fileToCopy === path.basename(file)) {
-    return console.log(
-      `Unable to proceed. File: ${targetPath} doesn't exist in derictory!`
-    );
-  }
-
-  return console.log(
-    `Unable to proceed. File: ${fileToCopy} doesn't exist in derictory!`
-  );
-}
-
-if (path.dirname(file) === path.dirname(targetFolder)) {
-  return;
-};
-
-const targetDir = path.dirname(targetFolder);
-
-const targetFilePath = path.join(targetDir, fileToCopy);
-
-fs.copyFile(file, targetFilePath, (err) => {
+fs.copyFile(sourceFile, targetFilePath, (err) => {
   if (err) {
-    console.error(`Ошибка при копировании файла: ${err}`);
+    console.error(`There is an error with copying the file: ${err}`);
   } else {
-    console.log(`Файл успешно скопирован в ${targetFilePath}`);
+    console.log(`File ${baseNameFile} successfully copied in ${dirPath}`);
   }
 });
