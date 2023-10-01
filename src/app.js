606c2ce6ffdac5c142c26a75c20cd0d617bb5dfe
Oleksandr Lytovchenko <jaster93him@gmail.com>
2023-10-01 03:25:01 +0300

    Copying a file with error checking created

--- src/app.js
+++ src/app.js
@@ -1,1 +1,39 @@
+/* eslint-disable no-console */
 'use strict';
+
+const fs = require('fs');
+const path = require('path');
+
+const params = process.argv.slice(2);
+
+if (params.length !== 2) {
+  console.log('Incorrect parameters');
+
+  return;
+}
+
+const addressFile = path.join(__dirname, params[0]);
+const addressCopy = path.join(__dirname, params[1]);
+
+try {
+  fs.readFileSync(addressFile, 'utf-8');
+} catch (err) {
+  console.log('File not found');
+
+  return;
+}
+
+try {
+  fs.readFileSync(addressCopy, 'utf-8');
+  console.log('File exists');
+
+  return;
+} catch (err) {}
+
+const content = fs.readFileSync(addressFile, 'utf-8');
+
+try {
+  fs.writeFileSync(addressCopy, content);
+} catch (err) {
+  console.log('Somme error...');
+}
