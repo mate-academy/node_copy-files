1012fe42eec1b482b443d3f4f9386d65087c5dc6
Taras Voievoda <voevoda.taras@gmail.com>
2023-10-03 15:56:08 +0300

    added logic for copying files

--- src/app.js
+++ src/app.js
@@ -1,1 +1,35 @@
+/* eslint-disable no-console */
 'use strict';
+
+const fs = require('fs');
+
+function copyFile(currentLocation, specifiedLocation) {
+  if (currentLocation === specifiedLocation) {
+    console.log('You trying to copy to the same location.');
+
+    return;
+  }
+
+  const contentFromCurrentLocation = fs.readFileSync(currentLocation, 'utf-8');
+
+  fs.writeFileSync(
+    specifiedLocation,
+    contentFromCurrentLocation,
+    'utf-8',
+    (err) => {
+      if (err) {
+        console.log(err);
+
+        return;
+      }
+
+      return `File copied to ${specifiedLocation}`;
+    }
+  );
+
+  console.log(contentFromCurrentLocation);
+}
+
+module.exports = {
+  copyFile,
+};
