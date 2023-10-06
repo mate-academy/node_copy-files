c1e0fb53c36855dc7d766006b62108378fc2a6b4
Denis Chakhalian <my.chahalyan@gmail.com>
2023-10-06 16:56:31 +0300

    realized copy any file to another location

--- src/app.js
+++ src/app.js
@@ -1,1 +1,24 @@
 'use strict';
+const fs = require('fs');
+
+const fileToCopy = process.argv[2];
+const copyOfFile = process.argv[3];
+
+async function copy(fileToCopy, copyOfFile) {
+  console.log('Copying...');
+
+  if (fileToCopy === copyOfFile) {
+    console.log('You chose the same file name');
+    return;
+  }
+
+  try {
+    await fs.copyFileSync(fileToCopy, copyOfFile);
+
+    console.log('Copy is done');
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+copy(fileToCopy, copyOfFile);
