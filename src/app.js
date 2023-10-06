a1186175991e9092c392edfb548bd4e1c73d9a38
daviinnchi <zaikoivan99@gmail.com>
2023-10-06 16:33:06 +0200

    Created copying files app

--- src/app.js
+++ src/app.js
@@ -1,1 +1,22 @@
+/* eslint-disable no-console */
 'use strict';
+
+const fs = require('fs');
+
+function copy(source, dest) {
+  if (source === dest) {
+    console.log('Can \'t copy to same location');
+
+    return;
+  }
+
+  try {
+    fs.copyFileSync(source, dest);
+
+    console.log('Copied');
+  } catch (e) {
+    console.log('Something went wrong...', e);
+  }
+}
+
+module.exports = { copy };
