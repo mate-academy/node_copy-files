c4f8689559e086de08ce81b2b39a7452dae3d839
Sergey Mironenko <serg1prime@gmail.com>
2023-10-08 02:02:07 +0300

    Node copy files first commit

--- src/app.js
+++ src/app.js
@@ -1,1 +1,28 @@
 'use strict';
+
+const fs = require('fs/promises');
+
+const operation = process.argv[2];
+
+const cp = async () => {
+  const currPath = process.argv[3];
+  const newPath = process.argv[4];
+
+  if (currPath === newPath) {
+    console.log('Pathes are same');
+  } else {
+    try {
+      const response = await fs.readFile(currPath, 'utf-8');
+
+      await fs.writeFile(newPath, response);
+    } catch {
+      console.log(error);
+    }
+  }
+};
+
+if (operation === 'cp') {
+  cp();
+} else {
+  console.log('No such operation');
+};
