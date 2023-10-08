320f3eb2e875c60d7dac1e859d92a15e9dc82025
neovendano <kolesnyk.vik@gmail.com>
2023-10-08 19:59:02 +0300

    add copy function

--- src/app.js
+++ src/app.js
@@ -1,1 +1,33 @@
+/* eslint-disable no-console */
 'use strict';
+
+const fs = require('fs');
+const path = require('path');
+
+function copyFile(source, destination) {
+  if (path.resolve(source) === path.resolve(destination)) {
+    console.error('Source and destination paths are the same.');
+
+    return;
+  }
+
+  if (!fs.existsSync(source)) {
+    console.error(`Source file "${source}" does not exist.`);
+
+    return;
+  }
+
+  fs.copyFileSync(source, destination);
+  console.log(`File copied from "${source}" to "${destination}"`);
+}
+
+const args = process.argv.slice(2);
+
+if (args.length !== 2) {
+  console.error('Usage: node app.js <source> <destination>');
+  process.exit(1);
+}
+
+const [src, dest] = args;
+
+copyFile(src, dest);
