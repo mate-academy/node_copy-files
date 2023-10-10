8c52e7e958655364fc9be4a161abca1cd47fdc74
Oleh Hurmanchuk <gyrmanchyk230506@gmail.com>
2023-10-10 17:49:05 +0300

    implement file copying

--- src/app.js
+++ src/app.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const fs = require('fs');
+
+function copyFile() {
+  const [fileToCopy, fileToCreate] = process.argv.slice(2);
+
+  if (fileToCopy === fileToCreate) {
+    return;
+  }
+
+  const fileContent = fs.readFileSync(fileToCopy, 'utf8');
+
+  fs.writeFileSync(fileToCreate, fileContent);
+}
+
+copyFile();
