deb18e08d1ce5e460f003e99c94235da3a421883
Rostyslav Kravchuk <mr.shadov.05@gmail.com>
2023-10-11 09:53:40 +0300

    add warn messages, improve program logic (now we can work with it in the console)

--- src/app.js
+++ src/app.js
@@ -1,5 +1,15 @@
 'use strict';
 
+const minimist = require('minimist');
 const { makeFileCopy } = require('./makeFileCopy');
 
-makeFileCopy('someTextToCopy.txt', '../Copy1.txt');
+function startProgram() {
+  const { _: args } = minimist(process.argv.slice(2));
+
+  const copiedFilePath = args[0];
+  const newFilePath = args[1];
+
+  makeFileCopy(copiedFilePath, newFilePath);
+};
+
+startProgram();
--- src/makeFileCopy.js
+++ src/makeFileCopy.js
@@ -1,4 +1,5 @@
 'use strict';
+/* eslint-disable */
 
 const fs = require('fs');
 const path = require('path');
@@ -16,12 +17,16 @@ function makeFileCopy(fileToCopy, newFilePath) {
   const pathOfCopiedFile = path.join(__dirname, newFilePath);
 
   if (getDirectoryPath(fileToCopyPath) === getDirectoryPath(pathOfCopiedFile)) {
+    console.warn('You cannot copy the file to the same directory');
+
     return;
   }
 
-  const fileData = fs.readFileSync(fileToCopyPath, 'utf-8');
+  const fileData = fs.readFileSync(fileToCopyPath);
 
   fs.writeFileSync(pathOfCopiedFile, fileData);
+
+  console.log(`Your file has been copied successfully to ${pathOfCopiedFile}`);
 };
 
 module.exports = {
