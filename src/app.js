52d4f2f7772d3296f7b8305624f82de6346d5654
KatOlista^^! <olya.katyushina@gmail.com>
2023-10-11 20:42:34 +0200

    add constants.js with constant address

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
 'use strict';
 
+const ADDRESS = require('./constants');
 const copyFile = require('./copyFile');
+
+const [addressFrom, addresTo] = ADDRESS.slice(3).split(' ');
 
 copyFile(addressFrom, addresTo);
---
+++ src/constants.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const ADDRESS = 'cp file.txt file-copy.txt';
+
+module.exports = {
+  ADDRESS,
+};
