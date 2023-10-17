858877672ae964789b72b4ba13094e92030d4666
Andrzej Pietkiewicz <pietan@me.com>
2023-10-17 14:29:46 +0200

    Solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const fs = require('fs');
+
+const { command, from, to } = process.argv;
+
+console.log(command, from, to);
+
+if (command === 'copy' && from !== to) {
+  fs.writeFile(fs.readFile(from));
+  fs.rm(from);
+}
