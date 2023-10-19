78fb5cee9097c15009db3d1ed003487cdafeb36a
Capoverde <piotr_as@yahoo.com>
2023-10-19 15:33:14 +0200

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,32 @@
 'use strict';
+
+const fs = require('fs');
+const path = require('path');
+
+function copyFile(sourcePath, destinationPath) {
+  const sourceAbsolutePath = path.resolve(sourcePath);
+  const destinationAbsolutePath = path.resolve(destinationPath);
+
+  if (sourceAbsolutePath === destinationAbsolutePath) {
+    // console.log('Source and destination paths are the same. Nothing to do.');
+
+    return;
+  }
+
+  try {
+    const fileContent = fs.readFileSync(sourceAbsolutePath);
+
+    fs.writeFileSync(destinationAbsolutePath, fileContent);
+
+    return `File copied from ${sourceAbsolutePath} 
+    to ${destinationAbsolutePath}`;
+  } catch (error) {
+    throw Error(`Error occurred while copying the file: ${error.message}`);
+  }
+}
+
+// example
+const sourceFilePath = 'file.txt';
+const destinationFilePath = 'file-copy.txt';
+
+copyFile(sourceFilePath, destinationFilePath);
