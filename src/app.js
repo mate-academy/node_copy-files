8d01aa47a4a876c552b6061bbdae95c807aab24e
Mykola Plekan <zikzar@gmail.com>
2023-10-25 13:37:28 +0300

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const fs = require('fs');
+
+const [from, to] = process.argv.slice(2);
+
+if (from === to) {
+  throw new Error('It is the same file, need a different one');
+}
+
+fs.readFile(from, (err, data) => {
+  if (err) {
+    throw err;
+  }
+
+  fs.appendFile(to, data.toString(), (error) => {
+    if (error) {
+      throw error;
+    }
+
+    return 'Done';
+  });
+});
