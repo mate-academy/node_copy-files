7eaea8949de0246192d5310e623a6ec108c07c9b
Mykola Plekan <zikzar@gmail.com>
2023-10-25 15:39:19 +0300

    solution

--- src/app.js
+++ src/app.js
@@ -8,16 +8,10 @@ if (from === to) {
   throw new Error('It is the same file, need a different one');
 }
 
-fs.readFile(from, (err, data) => {
-  if (err) {
-    throw err;
+fs.copyFile(from, to, (error) => {
+  if (error) {
+    throw error;
   }
 
-  fs.appendFile(to, data.toString(), (error) => {
-    if (error) {
-      throw error;
-    }
-
-    return 'Done';
-  });
+  return 'Done';
 });
