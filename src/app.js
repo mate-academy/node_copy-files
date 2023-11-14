1e19160d0691d5b8c0c4ca59b9d2ce13c26ae916
diamat1997 <diamat1997@gmail.com>
2023-11-14 14:56:24 +0200

    add task solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const fs = require('fs');
+
+function copyFile(sourse, destination) {
+  try {
+    fs.copyFileSync(sourse, destination);
+  } catch (e) {
+    throw new Error(e);
+  }
+}
+
+copyFile(process.argv[2], process.argv[3]);
