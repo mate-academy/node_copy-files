73b0cb778759c17374fe5675f71ec0b61d7de3d7
Nazar Hnatyuk <108331415+Stim1@users.noreply.github.com>
2023-11-27 16:59:51 +0200

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,31 @@
+/* eslint-disable no-console */
 'use strict';
+
+const fs = require('fs');
+const path = require('path');
+
+function copyFile(sourcePath, destinationPath) {
+  const absoluteSourcePath = path.resolve(sourcePath);
+  const absoluteDestinationPath = path.resolve(destinationPath);
+
+  if (absoluteSourcePath === absoluteDestinationPath) {
+    console.log('Source and destination are the same');
+
+    return;
+  }
+
+  try {
+    const fileContent = fs.readFileSync(absoluteSourcePath);
+
+    fs.writeFileSync(absoluteDestinationPath, fileContent);
+
+    console.log(`File copied successfully`);
+  } catch (error) {
+    console.log('Error copying a file');
+  }
+}
+
+const sourceFilePath = 'test.txt';
+const destinationFilePath = 'test2.txt';
+
+copyFile(sourceFilePath, destinationFilePath);
