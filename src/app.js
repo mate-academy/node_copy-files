ffec4e3b29f42f314bbda50379f61ac2971a0342
Sviatoslav Kondur <sviatikkondur5@gmail.com>
2023-11-27 18:14:51 +0100

    fixed logic

--- src/app.js
+++ src/app.js
@@ -2,50 +2,29 @@
 
 'use strict';
 
-const readline = require('readline');
 const fs = require('fs').promises;
 const chalk = require('chalk');
 
-const terminal = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-function setPromptAndPrompt() {
-  terminal.setPrompt(
-    chalk.blue('Enter the source and destination separated by a space: ')
-  );
-  terminal.prompt();
-}
-
-async function handleInput(input) {
-  const [source, destination] = input.split(' ');
-
+async function handleInput(source, destination) {
   if (source === destination) {
     console.log(chalk.yellow('Source and destination should be different'));
-
-    terminal.close();
-
-    return;
   }
 
   try {
     await fs.copyFile(source, destination);
     console.log(chalk.bgGreen('Copied ' + source + ' to ' + destination));
-    terminal.close();
   } catch (err) {
     console.log(chalk.bgRed('Falied... Check your source and destination...'));
-    setPromptAndPrompt();
   }
 }
 
 const app = () => {
-  console.log(`Current directory - ${__dirname}`);
-  console.log(`Example of prompt - ./src/source/1.txt ./src/destination/1.txt`);
+  // eslint-disable-next-line max-len
+  console.log('Example of prompt - node ./src/app ./src/source/1.txt ./src/destination/1.txt');
 
-  setPromptAndPrompt();
+  const [source, destination] = process.argv.slice(2);
 
-  terminal.on('line', handleInput);
+  handleInput(source, destination);
 };
 
 app();
