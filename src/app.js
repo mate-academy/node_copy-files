10e71ce1cd04d3a7a5bddfd7031309dffb65c3bc
danulyk05 <ivan.danulyk2016@gmail.com>
2023-11-28 17:54:20 +0100

    solution2

--- src/app.js
+++ src/app.js
@@ -3,24 +3,20 @@
 
 const fs = require('fs');
 const path = require('path');
+const magicNumber = process.argv.length !== 4;
 
-// eslint-disable-next-line no-shadow
-function copyFile(sourcePath, destinationPath) {
-  // Resolve the absolute paths
-  const sourceAbsolutePath = path.resolve(sourcePath);
-  const destinationAbsolutePath = path.resolve(destinationPath);
+function copyFile(sourcePathOfFile, destinationPathOfFile) {
+  const sourceAbsolutePath = path.resolve(sourcePathOfFile);
+  const destinationAbsolutePath = path.resolve(destinationPathOfFile);
 
-  // Check if source and destination are the same
   if (sourceAbsolutePath === destinationAbsolutePath) {
     console.log('Source and destination are the same. No action taken.');
 
     return;
   }
 
-  // Read the content from the source file
   const fileContent = fs.readFileSync(sourceAbsolutePath);
 
-  // Write the content to the destination file
   fs.writeFileSync(destinationAbsolutePath, fileContent);
 
   console
@@ -30,14 +26,11 @@ function copyFile(sourcePath, destinationPath) {
     );
 }
 
-// Check if the correct number of arguments is provided
-if (process.argv.length !== 4) {
+if (magicNumber) {
   console.error('Usage: node copyFile.js <sourcePath> <destinationPath>');
   process.exit(1);
 }
 
-// Get the source and destination paths from command line arguments
 const [, , sourcePath, destinationPath] = process.argv;
 
-// Call the copyFile function with provided paths
 copyFile(sourcePath, destinationPath);
