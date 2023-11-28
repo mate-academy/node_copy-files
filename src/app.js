68d672ecbcb010ed67c18959427cd7237541be05
Serhii Mykhailivskyi <sergeymih098123@gmail.com>
2023-11-28 12:55:39 +0200

    add task solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,25 @@
 'use strict';
+
+const fs = require('fs/promises');
+
+const app = async() => {
+  const [source, destination] = process.argv.slice(2);
+
+  if (source === destination) {
+    // eslint-disable-next-line no-console
+    console.log(
+      `Expected two different paths, recieved:\n${source}\n${destination}`
+    );
+
+    return;
+  }
+
+  try {
+    await fs.copyFile(source, destination);
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.log('Failed to copy. Check your paths.');
+  }
+};
+
+app();
