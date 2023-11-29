a2a867c35611965d9c5fc2c6d9213a97c347e72a
Nazar Hnatyuk <108331415+Stim1@users.noreply.github.com>
2023-11-29 15:06:57 +0200

    added form console input

--- src/app.js
+++ src/app.js
@@ -3,6 +3,12 @@
 
 const fs = require('fs');
 const path = require('path');
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
 function copyFile(sourcePath, destinationPath) {
   const absoluteSourcePath = path.resolve(sourcePath);
@@ -16,14 +22,15 @@ function copyFile(sourcePath, destinationPath) {
     const fileContent = fs.readFileSync(absoluteSourcePath, 'utf-8');
 
     fs.writeFileSync(absoluteDestinationPath, fileContent, 'utf-8');
-
     console.log('File copied successfully');
   } catch (error) {
-    throw new Error('Error copying a file');
+    console.error('Error copying a file:', error);
   }
 }
 
-const sourceFilePath = 'test.txt';
-const destinationFilePath = 'test2.txt';
-
-copyFile(sourceFilePath, destinationFilePath);
+rl.question('Enter the source file path: ', (sourceFilePath) => {
+  rl.question('Enter the destination file path: ', (destinationFilePath) => {
+    copyFile(sourceFilePath, destinationFilePath);
+    rl.close();
+  });
+});
