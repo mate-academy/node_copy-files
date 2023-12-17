681d2e9a1539e2fb067698d180f3ca2ab7c71c9d
Leonid Krainik <rialleons@gmail.com>
2023-12-18 00:18:34 +0200

    Write down app to copy files

--- src/app.js
+++ src/app.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const fs = require('fs');
+
+const copy = () => {
+  if (process.argv[2] === process.argv[3]) {
+    return;
+  }
+
+  try {
+    const data = fs.readFileSync(process.argv[2], 'utf-8');
+
+    fs.writeFile(process.argv[3], data, 'utf-8', () => {});
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+module.exports = {
+  copy,
+};
