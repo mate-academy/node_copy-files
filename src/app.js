7ecd03d8346d9a3d45943031f38792a9cb664aab
Yehor Korablin <yegor_ka2004@ukr.net>
2023-12-28 12:06:31 +0200

    Added solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const fs = require('fs');
+
+const cp = (fileSrc, fileDes) => {
+  if (fileSrc === fileDes) {
+    return;
+  }
+
+  const copy = fs.readFileSync(fileSrc, { encoding: 'utf-8' });
+
+  fs.writeFile(fileDes, copy);
+};
