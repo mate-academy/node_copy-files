89043be63249f783a711d150c73e191c2874d2c6
Vo7kov <jegor.volkov.a@gmail.com>
2024-01-04 19:11:13 +0200

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,25 @@
 'use strict';
+
+const fs = require('fs');
+
+const copy = (pathToInitialFile, pathToFinalFile) => {
+  const content = fs.readFileSync(pathToInitialFile, 'utf-8');
+
+  const inititalDirectory = pathToInitialFile.slice(
+    0,
+    pathToInitialFile.split('').lastIndexOf('/')
+  );
+  const finalDirectory = pathToFinalFile.slice(
+    0,
+    pathToFinalFile.split('').lastIndexOf('/')
+  );
+
+  if (inititalDirectory === finalDirectory) {
+    return;
+  }
+
+  fs.writeFileSync(pathToFinalFile, content);
+  fs.unlinkSync(pathToInitialFile);
+};
+
+copy('src/app.js', 'src/test/test.txt');
