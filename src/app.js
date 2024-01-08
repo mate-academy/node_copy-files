1d82d23b4e481afad2c79a8f7956056d6342815b
Wita-Shchurko <vitaliya.mememe@gmail.com>
2024-01-08 15:33:28 +0200

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const fs = require('fs/promises');
+
+const copy = async(copyFrom, copyTo) => {
+  try {
+    if (copyFrom !== copyTo) {
+      const content = await fs.readFile(copyFrom, 'utf8');
+      fs.writeFile(copyTo, content, 'utf8');
+    }
+  } catch (error) {}
+
+};
+
+const [copyFrom, copyTo] = process.argv.slice(2);
+
+copy(copyFrom, copyTo);
