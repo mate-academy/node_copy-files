2919c64012866e9ed40b3a3d1e981f0616b94750
«Maria» <nedymai@gmail.com>
2024-01-08 09:08:43 +0200

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const fs = require('fs/promises');
+
+async function copy(src, dist) {
+  try {
+    const data = await fs.readFile(src, 'utf-8');
+
+    await fs.writeFile(dist, data);
+  } catch (err) {
+  }
+}
+
+module.exports = {
+  copy,
+};
