f7d6cc4cef7e5383720fe0ae64d3a201151b929b
Kseniia Nikolaienko <ksysha.nikolaenko778@gmail.com>
2024-01-09 17:39:32 +0200

    fix

--- src/app.js
+++ src/app.js
@@ -1,9 +1,17 @@
 'use strict';
 
+const path = require('path');
 const { copyFile } = require('./copyFile');
 
 if (!process.argv.slice(2).length) {
   console.log('Enter source location and new file location');
 } else {
-  copyFile(process.argv[2], process.argv[3]);
+  const sourcePath = path.resolve(process.argv[2]);
+  const newFilePath = path.resolve(process.argv[3]);
+
+  if (sourcePath === newFilePath) {
+    console.log('Enter new file location');
+  } else {
+    copyFile(sourcePath, newFilePath);
+  }
 }
