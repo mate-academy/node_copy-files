7a4ed84dc0963b098ee21a7889a69a1b2f6f8fc9
Kseniia Nikolaienko <ksysha.nikolaenko778@gmail.com>
2024-01-09 17:32:23 +0200

    add task solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const { copyFile } = require('./copyFile');
+
+if (!process.argv.slice(2).length) {
+  console.log('Enter source location and new file location');
+} else {
+  copyFile(process.argv[2], process.argv[3]);
+}
