edaee950295ff1a02d6524aec5495372c5317bbd
Alina <kozlovska.a.mail@gmail.com>
2024-01-18 20:52:31 +0100

    add task solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,23 @@
+/* eslint-disable no-console */
 'use strict';
+
+const fs = require('fs/promises');
+
+function copyFile() {
+  const args = process.argv.slice(2);
+  const [source, dest] = args;
+
+  if (source === dest) {
+    return;
+  }
+
+  try {
+    fs.copyFile(source, dest);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+copyFile();
+
+module.exports = { copyFile };
