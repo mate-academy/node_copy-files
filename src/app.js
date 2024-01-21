1f1de8841fd8a769e8eb6bc5dd2910255d5d265a
Patrykmclaren14 <wierzbowskipatryk5@gmail.com>
2024-01-21 19:55:39 +0100

    Solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const fs = require('fs');
+
+const [, , file, fileToCopy] = process.argv;
+
+fs.readFile(`./src/${file}`, 'utf-8', (error, data) => {
+  if (error) {
+    // eslint-disable-next-line no-console
+    console.log(error);
+  }
+
+  fs.writeFile(`./src/${fileToCopy}`, data, (secondError) => {
+    if (error) {
+      // eslint-disable-next-line no-console
+      console.log(secondError);
+    }
+  });
+});
