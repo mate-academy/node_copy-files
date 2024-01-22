80807fae5eeb1f9a5bcb2df3364c5eb39fca12f9
Katsiaryna Prakofyeva <catherine.smiff@gmail.com>
2024-01-22 16:31:46 +0100

    added copy function fot copied content from file to another file

--- src/app.js
+++ src/app.js
@@ -1,1 +1,25 @@
+/* eslint-disable no-console */
 'use strict';
+
+const fs = require('fs');
+
+const copy = async(fromPath, toPath) => {
+  if (fromPath === toPath) {
+    throw new Error('The same path');
+  }
+
+  try {
+    const content = await fs.readFileSync(fromPath, 'utf-8');
+
+    fs.writeFileSync(toPath, content);
+    console.log('Done');
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+const [from, to] = process.argv.slice(2);
+
+console.log(from, to);
+
+copy(from, to);
