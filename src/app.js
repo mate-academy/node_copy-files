0e5d4ab08ba7082465b40b7b98f92e0bd4b44064
Patrykmclaren14 <wierzbowskipatryk5@gmail.com>
2024-01-22 21:00:49 +0100

    Fixed solution

--- src/app.js
+++ src/app.js
@@ -4,6 +4,10 @@ const fs = require('fs');
 
 const [, , file, fileToCopy] = process.argv;
 
+if (file === fileToCopy) {
+  throw new Error('Files are the same');
+}
+
 fs.readFile(`./src/${file}`, 'utf-8', (error, data) => {
   if (error) {
     // eslint-disable-next-line no-console
