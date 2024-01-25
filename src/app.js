32c0fd299e6ddea53012b974a3068f4e14280cae
jacekkosiba <jac.kosiba@gmail.com>
2024-01-25 13:06:32 +0100

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,26 @@
 'use strict';
+
+const fs = require('fs');
+const path = require('path');
+const { argv } = require('process');
+
+function copyFiles(file, destination) {
+  if (!file || !destination) {
+    throw new Error('Provide filepaths');
+  }
+
+  const filePath = path.join(__dirname, file);
+  const destinationPath = path.join(__dirname, destination);
+
+  if (filePath === destinationPath) {
+    return;
+  }
+
+  fs.copyFile(filePath, destinationPath, (err) => {
+    throw new Error(err);
+  });
+}
+
+const [pathToCopy, pathToPaste] = argv.slice(2);
+
+copyFiles(pathToCopy, pathToPaste);
