0891f9c8565b3294bd6c46c485ce629e5fc3ac9f
Mykhailo <eglet777@ukr.net>
2024-01-26 16:46:50 +0200

    add task solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const fs = require('fs');
+
+const copyFile = (src, dest) => {
+  if (src === dest || !src || !dest) {
+    return;
+  }
+
+  const stats = fs.statSync(src);
+
+  if (!stats.isFile()) {
+    return;
+  }
+
+  fs.copyFileSync(src, dest);
+};
+
+copyFile(...process.argv.slice(2));
