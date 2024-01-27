a319c8a049d19ce04b4afa97d7fb3d74ae6926f6
Antonio S <soi4.toxa@ukr.net>
2024-01-27 08:14:14 +0200

    Implemented with switch

--- src/app.js
+++ src/app.js
@@ -1,1 +1,22 @@
+/* eslint-disable no-console */
 'use strict';
+
+const fs = require('fs');
+const [,, sourceFile, destinationFile] = process.argv;
+
+switch (true) {
+  case (process.argv.length !== 4):
+    console.error('You specified incorrect data');
+    break;
+
+  case (sourceFile === destinationFile):
+    break;
+
+  default: {
+    try {
+      fs.cpSync(sourceFile, destinationFile);
+    } catch (err) {
+      console.error('Error copying file: ', err);
+    }
+  }
+}
