e6fecd2ad2ee58fef44e88f29869d2ad468f1af9
Nazar Dudnyk <dudnyk_ak17@nuwm.edu.ua>
2024-02-23 14:20:03 +0200

    Solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,20 @@
+/* eslint-disable no-console */
 'use strict';
+
+const fs = require('fs/promises');
+
+const copy = async(pathFrom, pathTo) => {
+  try {
+    if (pathFrom !== pathTo) {
+      const content = await fs.readFile(pathFrom, 'utf8');
+
+      await fs.writeFile(pathTo, content, 'utf8');
+    }
+  } catch (error) {
+    console.error('Error occurred:', error);
+  }
+};
+
+const [copyFrom, copyTo] = process.argv.slice(2);
+
+copy(copyFrom, copyTo);
