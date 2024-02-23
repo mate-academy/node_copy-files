039ea26145081c771c277b92df06131c0ee06a91
Denis Kononenko <deniskononenko88@gmail.com>
2024-02-23 10:39:19 +0200

    fix solution 3

--- src/modules/customCopyFile.js
+++ src/modules/customCopyFile.js
@@ -1,32 +1,19 @@
+/* eslint-disable no-console */
 'use strict';
 
 const fs = require('fs');
-const path = require('path');
 
 const customCopyFile = (sourceFile, destinationFile) => {
   if (destinationFile === undefined) {
-    process.stderr.write('should be 2 params');
+    console.error('should be 2 params');
 
     return;
   }
-
-  if (sourceFile === destinationFile) {
-    return;
-  }
-
-  const sourcePath = path.join(__dirname, '..', '..', sourceFile);
-  const destinationPath = path.join(__dirname, '..', '..', destinationFile);
 
   try {
-    fs.copyFileSync(sourcePath, destinationPath);
+    fs.copyFileSync(sourceFile, destinationFile);
   } catch (err) {
-    if (err.code === 'EISDIR' || err.code === 'EPERM') {
-      process.stderr.write('source or destination is a directory');
-    }
-
-    if (err.code === 'ENOENT') {
-      process.stderr.write('non-existent source file');
-    }
+    console.error(err);
   }
 };
 
