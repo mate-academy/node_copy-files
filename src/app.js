94eb99045b3b82b25664e67dc707cb82422be1b1
Dmytro Levchenko <levchenkod91@gmail.com>
2024-02-26 14:59:16 +0200

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,20 @@
+/* eslint-disable no-console */
 'use strict';
+
+const fs = require('fs/promises');
+const [from, to] = process.argv.slice(2);
+
+async function copyFile(copyFrom, copyTo) {
+  try {
+    if (copyFrom !== copyTo) {
+      const content = await fs.readFile(copyFrom);
+
+      await fs.writeFile(copyTo, content);
+      console.log('Copy is done');
+    }
+  } catch (e) {
+    throw new Error(e);
+  }
+}
+
+copyFile(from, to);
