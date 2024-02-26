3790e497f915cb63dd7f4b709109fc508f2c79fb
Dmytro Levchenko <levchenkod91@gmail.com>
2024-02-26 15:06:13 +0200

    add utf-8 encoding

--- src/app.js
+++ src/app.js
@@ -7,9 +7,9 @@ const [from, to] = process.argv.slice(2);
 async function copyFile(copyFrom, copyTo) {
   try {
     if (copyFrom !== copyTo) {
-      const content = await fs.readFile(copyFrom);
+      const content = await fs.readFile(copyFrom, { encoding: 'utf-8' });
 
-      await fs.writeFile(copyTo, content);
+      await fs.writeFile(copyTo, content, { encoding: 'utf-8' });
       console.log('Copy is done');
     }
   } catch (e) {
