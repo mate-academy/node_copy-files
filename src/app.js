2149b5ca3349922ec434aca497cc4fcd0214357b
Alyona Nabok <alyonana42@gmail.com>
2024-03-04 13:46:13 +0200

    add condition

--- src/app.js
+++ src/app.js
@@ -10,6 +10,10 @@ function copyFile() {
       throw new Error('Both file and location arguments are required');
     }
 
+    if (file === location) {
+      throw new Error('Source and destination files are the same.');
+    }
+
     const content = fs.readFileSync(file, 'utf-8');
 
     fs.writeFileSync(location, content, 'utf-8');
