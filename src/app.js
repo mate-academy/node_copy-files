494c11a5cf7a5c2febdbd736e3342bc5b4ef5fdc
Alyona Nabok <alyonana42@gmail.com>
2024-03-04 13:43:40 +0200

    add console

--- src/app.js
+++ src/app.js
@@ -2,20 +2,20 @@
 'use strict';
 
 const fs = require('fs');
-
 const [file, location] = process.argv.slice(2);
 
 function copyFile() {
   try {
     if (!file || !location) {
-      throw new Error('Only one argument is provided');
+      throw new Error('Both file and location arguments are required');
     }
 
     const content = fs.readFileSync(file, 'utf-8');
 
     fs.writeFileSync(location, content, 'utf-8');
+    console.log('File copied successfully!');
   } catch (error) {
-    console.error(error);
+    console.error('Error occurred:', error);
   }
 }
 
