971d9ee633cdbaa160b0160c9063b3edba35b53f
Alyona Nabok <alyonana42@gmail.com>
2024-03-04 13:34:11 +0200

    add solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,24 @@
+/* eslint-disable no-console */
 'use strict';
+
+const fs = require('fs');
+
+const [file, location] = process.argv.slice(2);
+
+function copyFile() {
+  try {
+    if (!file || !location) {
+      throw new Error('Only one argument is provided');
+    }
+
+    const content = fs.readFileSync(file, 'utf-8');
+
+    fs.writeFileSync(location, content, 'utf-8');
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+copyFile();
+
+module.exports = { copyFile };
