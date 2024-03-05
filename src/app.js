a1922d87106395f25c0525f617f8ee6319296862
Dmytro Yarechuk <retrodn14@gmail.com>
2024-03-05 18:48:50 +0200

    add task solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,33 @@
+/* eslint-disable no-console */
 'use strict';
+
+const fs = require('fs');
+
+function createCopy(file, location) {
+  if (!file || !location) {
+    console.error('Both source file and destination must be provided');
+
+    return;
+  }
+
+  if (file === location) {
+    console.log('Source file and destination are the same');
+
+    return;
+  }
+
+  try {
+    const fileContent = fs.readFileSync(file, 'utf-8');
+
+    fs.writeFileSync(location, fileContent, 'utf-8');
+    console.log('File copied successfully');
+  } catch (error) {
+    console.log('An error occured: ', error);
+  }
+}
+
+createCopy(process.argv[2], process.argv[3]);
+
+module.exports = {
+  createCopy,
+};
