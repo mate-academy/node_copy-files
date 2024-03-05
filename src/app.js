0ba48c3c79db62091fdf4996c0c2b77fe3467cd7
Sergey Kirichenko <s.kirichenko@m-dev.org>
2024-03-05 20:28:32 +0200

    check solution with updated deps

--- src/app.js
+++ src/app.js
@@ -1,1 +1,32 @@
+/* eslint-disable no-console */
 'use strict';
+
+const fs = require('fs');
+const path = require('path');
+
+function copyFile(fileSourcePath, fileDestinationPath) {
+  if (!fileSourcePath || !fileDestinationPath) {
+    console.error('only one argument');
+
+    return;
+  }
+
+  if (fileSourcePath === fileDestinationPath) {
+    console.error('equal argument');
+
+    return;
+  }
+
+  const fullSource = path.resolve(fileSourcePath);
+  const fullDestination = path.resolve(fileDestinationPath);
+
+  fs.copyFile(fullSource, fullDestination, (err) => {
+    if (err) {
+      console.error(err);
+    }
+  });
+}
+
+const [source, destination] = process.argv.slice(2);
+
+copyFile(source, destination);
