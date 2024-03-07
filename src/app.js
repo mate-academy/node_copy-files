2a7b8371937a36b3e8c35fd9a4fc5d16341d99a7
Anton Kuchmasov <antek.ukraine@gmail.com>
2024-03-07 13:21:39 +0200

    done task copy-files

--- src/app.js
+++ src/app.js
@@ -1,1 +1,27 @@
+/* eslint-disable no-console */
 'use strict';
+
+const fs = require('fs/promises');
+const path = require('path');
+
+const [from, destination] = process.argv.slice(2);
+
+if (!from || !destination) {
+  console.error('Bad arguments!');
+  process.exit();
+}
+
+const fromPath = path.resolve(from);
+const destinationPath = path.resolve(destination);
+
+if (fromPath !== destinationPath) {
+  (async() => {
+    try {
+      const dataFromFile = await fs.readFile(from);
+
+      await fs.writeFile(destination, dataFromFile.toString());
+    } catch (err) {
+      console.error(err);
+    }
+  })();
+}
