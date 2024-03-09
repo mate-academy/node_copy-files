20e7b0a2c79acf4f44bb64a9cd0c8c936d570119
Yevhen <steplerjeka@gmail.com>
2024-03-09 16:12:19 +0200

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,33 @@
+/* eslint-disable no-console */
 'use strict';
+
+const fs = require('fs');
+
+function createCopy(source, destination) {
+  if (!source || !destination) {
+    console.error('Source file and destination must be provided');
+
+    return;
+  }
+
+  if (source === destination) {
+    console.error('Source file and destination are the same');
+
+    return;
+  }
+
+  try {
+    const fileContent = fs.readFileSync(source, 'utf-8');
+
+    fs.writeFileSync(destination, fileContent, 'utf-8');
+    console.log('File copied successfully');
+  } catch (error) {
+    console.error('An error occured: ', error);
+  }
+}
+
+createCopy(process.argv[2], process.argv[3]);
+
+module.exports = {
+  createCopy,
+};
