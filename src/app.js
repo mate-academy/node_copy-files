46d7189e5f6d079c008ed6c3bf290a41a99b69be
Marek Maciejewski <marekmaciejewski1982@gmail.com>
2024-03-11 23:52:55 +0100

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,26 @@
+/* eslint-disable no-console */
 'use strict';
+
+const fs = require('fs/promises');
+
+const [, , source, destination] = process.argv;
+
+if (!source || !destination) {
+  console.error('Usage: node src/app.js <source> <destination>');
+  process.exit();
+}
+
+if (source !== destination) {
+  copyFile();
+}
+
+async function copyFile() {
+  try {
+    const data = await fs.readFile(source);
+
+    await fs.writeFile(destination, data);
+    console.log('File has been copied');
+  } catch (error) {
+    console.error('Error:', error.message);
+  }
+}
