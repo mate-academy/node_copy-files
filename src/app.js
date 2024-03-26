8ffcac8532a8f9de23566713e2031ba32f2e28ca
Oleksandr <dedikad@gmail.com>
2024-03-27 00:24:33 +0100

    done

--- src/app.js
+++ src/app.js
@@ -1,1 +1,22 @@
+/* eslint-disable no-console */
 'use strict';
+
+const fs = require('fs');
+
+const [srcFile, destFile] = process.argv.slice(2);
+
+if (!destFile) {
+  console.error('Expect 2 arguments');
+}
+
+if (srcFile === destFile) {
+  console.error('File is already exist');
+}
+
+try {
+  const content = fs.readFileSync(srcFile, 'utf-8');
+
+  fs.writeFileSync(destFile, content, 'utf-8');
+} catch (error) {
+  console.error('Error: ', error);
+}
