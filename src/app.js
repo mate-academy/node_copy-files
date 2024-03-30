4087b1284c88b5b8adc5ce0ed17180cf29564f61
Anton Bilous <toxanteloxran@gmail.com>
2024-03-30 18:02:27 +0200

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,45 @@
 'use strict';
+/* eslint-disable no-console */
+
+// const path = require('path');
+const file1 = process.argv[2];
+const file2 = process.argv[3];
+
+const fs = require('fs');
+
+function copy(urlMain, urlCopy) {
+  if (urlMain === urlCopy) {
+    console.error('The links are the same');
+
+    return;
+  }
+
+  if (urlMain === undefined || urlCopy === undefined) {
+    console.error('One of the arguments is not defined');
+
+    return;
+  }
+
+  fs.copyFile(urlMain, urlCopy, (error) => {
+    if (error) {
+      console.error(error);
+    }
+  });
+}
+
+copy(file1, file2);
+
+// const readStream = fs.createReadStream();
+// const writeStream = fs.createWriteStream();
+
+// function copy(urlMain, urlCopy) {
+//   if (urlMain === urlCopy) {
+//     console.error('The links are the same');
+
+//     return;
+//   }
+
+//   readStream.pipe(writeStream);
+// }
+
+module.exports = { copy };
