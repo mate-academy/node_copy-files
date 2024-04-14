892dcf9896781edeef8d1275f45cedef90576ff8
Yehor-f2006 <151622806+Yehor-f2006@users.noreply.github.com>
2024-04-14 15:50:20 +0300

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,30 @@
 'use strict';
+
+import fs from 'fs';
+
+/* eslint-disable no-console */
+
+const copyPaste = () => {
+  const files = {
+    from: process.argv[2] || '',
+    to: process.argv[3] || '',
+  };
+
+  if (!files.from || !files.to) {
+    throw new Error('2 valid file paths are required');
+  }
+
+  fs.readFile(files.from, 'utf-8', (err, data) => {
+    if (err) {
+      console.error(err);
+    }
+
+    fs.writeFile(files.to, data, (error) => {
+      if (error) {
+        console.error(error);
+      }
+    });
+  });
+};
+
+copyPaste();
