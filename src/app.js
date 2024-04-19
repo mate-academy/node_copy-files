5528a0a5423c1bf29e8ce10114cc06c127e6bff6
OleksiiMishchenko <alex.mishch@gmail.com>
2024-04-19 23:31:30 +0300

    Copy files

--- src/app.js
+++ src/app.js
@@ -1,1 +1,23 @@
+/* eslint-disable no-console */
 'use strict';
+
+const fs = require('fs');
+
+function copyFiles() {
+  const params = process.argv.slice(2);
+  const [srcPath, destPath] = params;
+
+  if (srcPath === destPath) {
+    return;
+  }
+
+  try {
+    fs.copyFileSync(srcPath, destPath);
+
+    console.log('File copied successfully!');
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+copyFiles();
