4ff8269617af01ed750de134048464ba38c6eec7
unknown <bohdan.lesyk.kn.2021@lpnu.ua>
2024-04-24 21:16:23 +0300

    develop

--- src/app.js
+++ src/app.js
@@ -1,1 +1,31 @@
+/* eslint-disable no-console */
 'use strict';
+
+const fs = require('fs');
+
+function copyFile(srcFile, destFile) {
+  if (!destFile) {
+    console.error('2 arguments expected.');
+
+    return;
+  }
+
+  if (srcFile === destFile) {
+    console.error('Error: Source and destination are the same.');
+
+    return;
+  }
+
+  try {
+    const content = fs.readFileSync(srcFile, 'utf-8');
+
+    fs.writeFileSync(destFile, content, 'utf-8');
+    console.log(`File ${srcFile} copied to ${destFile} successfully.`);
+  } catch (error) {
+    console.error('Error: ', error);
+  }
+}
+
+const [source, destination] = process.argv.slice(2);
+
+copyFile(source, destination);
