00d4495806358c67577fb22797388f662f321b66
Nirvacsh <nir.vlad.zu@gmail.com>
2024-05-02 10:29:34 +0300

    Solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,38 @@
+/* eslint-disable no-console */
 'use strict';
+
+const fs = require('fs');
+const path = require('path');
+
+function copyFile(source, destination) {
+  if (path.resolve(source) === path.resolve(destination)) {
+    console.error('Error: Source and destination cannot be the same.');
+
+    return;
+  }
+
+  fs.copyFile(source, destination, (err) => {
+    if (err) {
+      console.error('Error copying file:', err);
+
+      return;
+    }
+    console.log(`File copied from ${source} to ${destination}`);
+  });
+}
+
+function main() {
+  const args = process.argv.slice(2);
+
+  if (args.length !== 2) {
+    console.error('Usage: node app.js <source> <destination>');
+
+    return;
+  }
+
+  const [source, destination] = args;
+
+  copyFile(source, destination);
+}
+
+main();
