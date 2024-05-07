c7a3c429659fd2e12339bfaa54d16cfc63504c97
OleksiiNesteruk <nesteruk.oleksiy@gmail.com>
2024-05-07 09:13:06 +0100

    Added task solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,20 @@
+/* eslint-disable no-console */
 'use strict';
+
+const fs = require('fs');
+
+function copyFile() {
+  const [source, destination] = process.argv.slice(2);
+
+  if (source === destination) {
+    return;
+  }
+
+  try {
+    fs.copyFileSync(source, destination);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+copyFile();
