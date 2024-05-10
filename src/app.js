a93ac732cc3435006de97f03df674d1f558c6e68
Olena Shapkun <shapkunkh@gmail.com>
2024-05-10 17:05:42 +0300

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,34 @@
 'use strict';
+/* eslint-disable no-console */
+
+const fs = require('fs');
+
+function copyFile() {
+  const [source, destination] = process.argv.slice(2);
+
+  console.log(process.argv.slice(2));
+
+  if (source === destination) {
+    console.error('Source should be different from destination');
+
+    return;
+  }
+
+  if (!source || !destination) {
+    console.error('Please provide source and destination paths.');
+
+    return;
+  }
+
+  fs.copyFile(source, destination, (error) => {
+    if (error) {
+      console.error('Something went wrong:', error);
+
+      return;
+    }
+
+    console.log('File copied successfully');
+  });
+}
+
+copyFile();
