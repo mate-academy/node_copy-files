e136ede9f690f0fcbbbe2349458e32a4db50adcf
Ярина Пугач <puhachyaryna@gmail.com>
2024-06-10 14:50:19 +0200

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,25 @@
 'use strict';
+/* eslint-disable no-console */
+
+const fs = require('fs/promises');
+
+const [, , source, destination] = process.argv;
+
+fs.lstat(destination)
+  .then((stats) => stats.isDirectory())
+  .catch(() => false)
+  .then((isDirectory) => {
+    if (!source || !destination || isDirectory) {
+      console.error();
+
+      return;
+    }
+
+    if (source !== destination) {
+      fs.readFile(source, 'utf-8')
+        .then((data) => fs.writeFile(destination, data, 'utf-8'))
+        .catch(() => {
+          console.error();
+        });
+    }
+  });
