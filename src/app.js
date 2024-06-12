cf1c590c1e12c6ee6ec7bc740164ab1b82dda4a5
Justin <thedarkvibe1@gmail.com.com>
2024-06-12 12:36:19 +0200

    done

--- src/app.js
+++ src/app.js
@@ -1,1 +1,47 @@
+/* eslint-disable no-console */
 'use strict';
+
+const fs = require('fs');
+const path = require('path');
+
+function app() {
+  if (process.argv.length < 4) {
+    console.error('Not enough args');
+
+    return;
+  }
+
+  const [from, to] = process.argv.slice(2, 4);
+
+  if (from === to) {
+    return;
+  }
+
+  if (!fs.existsSync(from)) {
+    console.error('DoesntExist');
+
+    return;
+  }
+
+  if (fs.lstatSync(from).isDirectory()) {
+    console.error('SourceIsDir');
+
+    return;
+  }
+
+  const toDir = path.dirname(to);
+
+  if (!fs.existsSync(toDir)) {
+    fs.mkdirSync(toDir, { recursive: true });
+  }
+
+  if (fs.existsSync(to) && fs.lstatSync(to).isDirectory()) {
+    console.error('Destination path is a directory.');
+
+    return;
+  }
+
+  fs.copyFileSync(from, to);
+}
+
+app();
