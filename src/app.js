706775a8a57818b44913cdce4a7c5794888c630e
eduvinagre <eduvinagre@gmail.com>
2024-06-25 11:11:52 -0300

    adding task solution

--- src/app.js
+++ src/app.js
@@ -1,4 +1,3 @@
-'use strict';
 /* eslint-disable no-console */
 'use strict';
 
@@ -6,14 +5,12 @@ const fs = require('fs');
 const path = require('path');
 
 function copyFile(src, dest) {
-  // Check if the source and destination are the same
   if (path.resolve(src) === path.resolve(dest)) {
     console.log('Source and destination are the same. No action taken.');
 
     return;
   }
 
-  // Copy the file, overwriting if the destination exists
   fs.copyFile(src, dest, fs.constants.COPYFILE_FICLONE, (err) => {
     if (err) {
       console.error('Error copying file:', err);
