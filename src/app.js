f4ae77994617d8ebd5de872bc60f7c4797173b83
Александр <0999353523ak@gmail.com>
2024-07-29 16:05:23 +0300

    Solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,31 @@
+/* eslint-disable no-console */
 'use strict';
+
+const fs = require('fs');
+
+function copyFile() {
+  const [from, to] = process.argv.slice(2);
+
+  if (from === to) {
+    console.error('Must be source and destination');
+
+    return;
+  }
+
+  if (!from || !to) {
+    console.error('Must be 2 arguments');
+
+    return;
+  }
+
+  try {
+    fs.copyFileSync(from, to);
+    console.log('Copy succeeded');
+  } catch (error) {
+    console.error('Copy failed:', error.message);
+  }
+}
+
+copyFile();
+
+module.exports = { copyFile };
