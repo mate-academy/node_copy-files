8bf43538d355224194cca663d3742c780ebe7f0d
Arthur Tolkachov <babanjerelo@gmail.com>
2024-08-01 14:26:03 +0300

    [Feature] added copy files app

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,18 @@
 module.exports = {
   extends: '@mate-academy/eslint-config',
   env: {
-    jest: true
+    jest: true,
   },
   rules: {
-    'no-proto': 0
+    'no-proto': 0,
+    'no-unused-vars': [
+      'error',
+      {
+        varsIgnorePattern: '^_',
+        caughtErrorsIgnorePattern: '^_',
+      },
+    ],
+    'no-console': ['error', { allow: ['error'] }],
   },
-  plugins: ['jest']
+  plugins: ['jest'],
 };
