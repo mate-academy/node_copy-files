d1ae2ebcf8aa529f606e6d9d419b6428800efc33
Patryk-Buczkowski <patryk.buczkowski@outlook.com>
2024-08-14 08:52:35 +0200

    commit

--- src/app.js
+++ src/app.js
@@ -5,13 +5,13 @@ const fs = require('fs');
 const args = process.argv.slice(2);
 const [source, destination] = args;
 
-if (args.length !== 2) {
-  console.error('Usage: node yourScript.js <source> <destination>');
-
-  process.exit(1);
-}
-
 function cp(location1, location2) {
+  if (args.length !== 2) {
+    console.error('Usage: node yourScript.js <source> <destination>');
+
+    return;
+  }
+
   try {
     fs.copyFileSync(location1, location2);
     console.log('File copied');
