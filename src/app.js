321d1c0ba572d68cfc1a591539e2dec46e10b6a7
Eater228 <543145yura@gmail.com>
2024-08-14 16:09:04 +0300

    Add task solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,39 @@
-'use strict';
+/* eslint-disable no-console */
+const fs = require('fs/promises');
+const path = require('path');
+
+async function copyFile(filesForCopy, pathToCopy) {
+  try {
+    if (path.resolve(filesForCopy) === path.resolve(pathToCopy)) {
+      console.error('Soure and destination are the same. No action taken');
+
+      return;
+    }
+
+    try {
+      await fs.access(filesForCopy);
+    } catch (err) {
+      console.error('Source file does not exist', err.message);
+
+      return;
+    }
+
+    await fs.cp(filesForCopy, pathToCopy);
+    console.log(`File coplied from ${filesForCopy} to ${pathToCopy}`);
+  } catch (err) {
+    console.error('Error during file copy', err.message);
+  }
+}
+
+const args = process.argv.slice(2);
+const [sourceFile, destinationFile] = args;
+
+if (args.length !== 2) {
+  console.error('Usage: node app.js <source> <destination>');
+}
+
+copyFile(sourceFile, destinationFile);
+
+module.exports = {
+  copyFile,
+};
