e09f2994b026a946077e9abce7ee2c23cbd2e596
SrTrace <levashov04sergey@gmail.com>
2024-09-05 13:31:27 +0300

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,39 @@
 'use strict';
+
+const fs = require('fs');
+const path = require('path');
+
+const paths = process.argv.slice(2);
+const [filePathOrig, filePathDest] = paths;
+
+if (paths.length !== 2) {
+  console.error('Not valid argument(s) is provided');
+} else {
+  copyFile(filePathOrig, filePathDest);
+}
+
+function copyFile(source, dest) {
+  const pathOrig = path.resolve(source);
+  const pathDest = path.resolve(dest);
+
+  if (pathOrig === pathDest) {
+    console.error('File in this directory already exist');
+
+    return;
+  }
+
+  if (!fs.existsSync(pathOrig)) {
+    console.error('This file does not exist', pathOrig);
+
+    return;
+  }
+
+  fs.copyFile(pathOrig, pathDest, (err) => {
+    if (err) {
+      console.error(`err`);
+
+      return;
+    }
+    console.log('File copied successfully.');
+  });
+}
