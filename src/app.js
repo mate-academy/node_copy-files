98dd86334b5a8df297159027532856d4b286124f
Vitalii Fedusov <vitalii.fedusov@gmail.com>
2024-10-01 23:47:41 +0300

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,26 @@
+/* eslint-disable no-console */
 'use strict';
+
+const fs = require('fs');
+
+function copyFiles() {
+  const [source, dest] = process.argv.slice(2);
+
+  if (!dest) {
+    console.error('Please provide two arguments');
+
+    return;
+  }
+
+  if (source === dest) {
+    return;
+  }
+
+  fs.cp(source, dest, (error) => {
+    if (error) {
+      console.error(error);
+    }
+  });
+}
+
+copyFiles();
