771081c08ea640fbf6960cd7b101961524eb4cd6
Vitalii Nezdoiminoha <vitalii.nezdojminoga@gmail.com>
2024-10-05 17:48:07 +0300

    added solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,27 @@
+/* eslint-disable no-console */
 'use strict';
+
+const fs = require('fs');
+
+const customCopyFile = () => {
+  const [from, to] = process.argv.slice(2);
+
+  if (!to) {
+    console.error('Error: Not enough second argument');
+
+    return;
+  }
+
+  if (to === from) {
+    return;
+  }
+
+  fs.cp(from, to, (err) => {
+    if (err) {
+      console.error(err);
+    }
+    console.log('Copied!');
+  });
+};
+
+customCopyFile();
