cc0a1e5d435de0b1ac45fb21bfd4755ad3e045e5
Andrii <andriykostiuk1@gmail.com>
2024-11-08 13:49:11 +0200

    add solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,36 @@
+/* eslint-disable no-console */
 'use strict';
+
+const fs = require('fs');
+
+const [sourcePath, destinationPath] = process.argv.slice(2);
+
+const copyfile = () => {
+  if (!sourcePath || !destinationPath) {
+    console.error('No source or destination provided');
+
+    return;
+  }
+
+  if (sourcePath === destinationPath) {
+    console.error('The paths are identical');
+
+    return;
+  }
+
+  const isExistSource = fs.existsSync(sourcePath);
+
+  if (!isExistSource) {
+    console.error('Non-existent source file');
+
+    return;
+  }
+
+  fs.copyFile(sourcePath, destinationPath, (err) => {
+    if (err) {
+      console.error('Something went wrong');
+    }
+  });
+};
+
+copyfile();
