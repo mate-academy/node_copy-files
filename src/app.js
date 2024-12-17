3c576de7c3f7a1c122aac6b305cd5b950ef7f5ea
Pavlo Lytvyshko <pavlo.litvishko@gmail.com>
2024-12-17 13:27:58 +0200

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,26 @@
+/* eslint-disable no-console */
 'use strict';
+
+const fs = require('fs');
+
+function copyFiles() {
+  const [fileSource, fileDestination] = process.argv.slice(2);
+
+  if (!fileDestination) {
+    console.error('Provide both file source path and file destination path');
+
+    return;
+  }
+
+  if (fileSource === fileDestination) {
+    return;
+  }
+
+  fs.cp(fileSource, fileDestination, (error) => {
+    if (error) {
+      console.error(error);
+    }
+  });
+}
+
+copyFiles();
