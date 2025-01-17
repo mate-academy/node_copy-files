242dc83eae5b01ae3e4dcd0d6c05372a0ad3e7f5
Anya Laban <laban.anya.a@gmail.com>
2025-01-17 19:13:55 +0200

    Solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,37 @@
+/* eslint-disable no-console */
 'use strict';
+
+const fs = require('fs/promises');
+const path = require('path');
+
+main();
+
+async function copyFileWithValidation(sourse, dest) {
+  if (sourse === dest) {
+    console.error('Шляхи до файлів однвкові');
+
+    return;
+  }
+
+  const soursePath = path.resolve(sourse);
+  const destPath = path.resolve(dest);
+
+  try {
+    await fs.copyFile(soursePath, destPath);
+    console.log('Файл успішно скопійований');
+  } catch (err) {
+    console.error('Помилка копіювання:', err);
+  }
+}
+
+async function main() {
+  const [sourcePath, destPath] = process.argv.slice(2);
+
+  if (!sourcePath || !destPath) {
+    console.error('Пропущено один або два шляхи до файлів');
+
+    return;
+  }
+
+  await copyFileWithValidation(sourcePath, destPath);
+}
