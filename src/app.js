01f45b549691dc27baa2ae65836b23438cfa1310
Gustavo Finch <gustavo.finch@gmail.com>
2025-01-30 18:19:04 -0300

    Solution GF

--- src/app.js
+++ src/app.js
@@ -1,1 +1,16 @@
+/* eslint-disable no-console */
 'use strict';
+
+import fs from 'fs';
+
+const [source, destination] = process.argv.slice(2);
+
+try {
+  if (source === destination) {
+    process.exit(0);
+  }
+
+  fs.copyFileSync(source, destination);
+} catch (error) {
+  console.error(`Error: ${error.message}`);
+}
