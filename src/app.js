395534bc8ff4f56304683106dab62678addb69cd
Gustavo Finch <gustavo.finch@gmail.com>
2025-01-30 18:26:16 -0300

    solution

--- src/app.js
+++ src/app.js
@@ -1,16 +1,42 @@
-/* eslint-disable no-console */
 'use strict';
 
-import fs from 'fs';
+const fs = require('fs').promises;
+const path = require('path');
 
-const [source, destination] = process.argv.slice(2);
+async function copyFiles(fileSource, fileDestination) {
+  try {
+    const fromPath = path.resolve(fileSource);
+    const toPath = path.resolve(fileDestination);
 
-try {
-  if (source === destination) {
-    process.exit(0);
+    if (fromPath === toPath) {
+      // eslint-disable-next-line no-console
+      console.error('Source and destination paths are the same');
+
+      return;
+    }
+
+    await fs.access(fromPath);
+    await fs.copyFile(fromPath, toPath);
+    // eslint-disable-next-line no-console
+    console.log(`File copied from ${fromPath} to ${toPath}`);
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error(`Error during file copy: ${error.message}`);
+  }
+}
+
+async function main() {
+  const args = process.argv.slice(2);
+  const [sourcePath, destinationPath] = args;
+
+  if (!sourcePath || !destinationPath) {
+    // eslint-disable-next-line no-console
+    console.error('One or both file paths are missing');
+
+    return;
   }
 
-  fs.copyFileSync(source, destination);
-} catch (error) {
-  console.error(`Error: ${error.message}`);
+  await copyFiles(sourcePath, destinationPath);
 }
+
+main();
