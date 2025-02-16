76c65fb68516b1b48e621a820aec6e1d151d4fb2
maxmodrr <maxmodrr@gmail.com>
2025-02-16 20:02:08 +0200

    results

--- src/app.js
+++ src/app.js
@@ -1,1 +1,17 @@
+/* eslint-disable no-console */
 'use strict';
+
+const fs = require('fs');
+
+const toCopy = process.argv[2];
+const toInsert = process.argv[3];
+
+if (toCopy !== toInsert) {
+  try {
+    const data = fs.readFileSync(toCopy, 'utf8');
+
+    fs.writeFileSync(toInsert, data);
+  } catch (error) {
+    console.error(error);
+  }
+}
