35dc4d0f047741a47c1e288570e3782f2f038e5b
Svitlana <svitlana.gaiduk@gmail.com>
2025-02-18 15:30:34 +0200

    add task solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,25 @@
+/* eslint-disable no-console */
 'use strict';
+
+const fs = require('fs');
+const path = require('path');
+
+function copyFile(source, destination) {
+  try {
+    if (path.resolve(source) === path.resolve(destination)) {
+      return;
+    }
+
+    if (!fs.statSync(source).isFile()) {
+      throw new Error('Source path must be a file.');
+    }
+
+    fs.copyFileSync(source, destination);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+  }
+}
+
+const [, , sourceFile, destinationFile] = process.argv;
+
+copyFile(sourceFile, destinationFile);
