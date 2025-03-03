eb54679bbe10fff1cec23191133d0b2a8cd0b59b
Vladislav Bezushko <bezushkovladis@gmail.com>
2025-03-03 17:20:47 +0200

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,15 @@
-'use strict';
+const fs = require('fs');
+
+const toCopy = process.argv[2];
+const toInsert = process.argv[3];
+
+if (toCopy !== toInsert) {
+  try {
+    const data = fs.readFileSync(toCopy, 'utf8');
+
+    fs.writeFileSync(toInsert, data);
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error(error);
+  }
+}
