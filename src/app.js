a50d6c5f2b35f9ed0060ba0b31bb9ea560335d4a
Maksym Korchynskyi <maksym.24.09.2003@gmail.com>
2025-03-10 15:10:30 +0200

    add task solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,27 @@
+/* eslint-disable no-console */
 'use strict';
+
+const fs = require('node:fs');
+
+if (process.argv.length !== 4) {
+  console.error('Bad request.');
+  process.exit();
+}
+
+const [sourceFile, destinationFile] = process.argv.splice(2);
+
+if (sourceFile !== destinationFile) {
+  try {
+    fs.writeFileSync(destinationFile, fs.readFileSync(sourceFile, 'utf8'));
+  } catch (err) {
+    switch (err.code) {
+      case 'EISDIR':
+        console.error('This is a directory.');
+        break;
+
+      case 'ENOENT':
+        console.error('No such file.');
+        break;
+    }
+  }
+}
