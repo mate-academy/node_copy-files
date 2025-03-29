d4620c51b5c9bd7438df5367585e0f4f59d17568
Bohdan Ivankov <bodanich600@gmail.com>
2025-03-29 13:18:48 +0100

    Solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,34 @@
+/* eslint-disable no-console */
 'use strict';
+
+const fs = require('fs');
+const path = require('path');
+
+function copy(source, destination) {
+  if (!fs.existsSync(source)) {
+    console.error(`Error: Source file "${source}" does not exist.`);
+
+    return;
+  }
+
+  if (path.resolve(source) === path.resolve(destination)) {
+    console.error(`Error: Source and destination are the same.`);
+
+    return;
+  }
+
+  fs.copyFile(source, destination, (err) => {
+    if (err) {
+      console.error(`Error: Failed to copy file. ${err.message}`);
+    }
+  });
+}
+
+const sourceFile = process.argv[2];
+const destinationFile = process.argv[3];
+
+if (!sourceFile || !destinationFile) {
+  console.error(`Please enter both source and destination file paths.`);
+} else {
+  copy(sourceFile, destinationFile);
+}
