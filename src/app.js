344d04aebd4c534ffd0523796a171e0e0d39ecd2
Taras Nechyporuck <tarasnechyporuck@gmail.com>
2025-03-30 16:50:07 +0300

    task done

--- src/app.js
+++ src/app.js
@@ -1,1 +1,19 @@
+/* eslint-disable no-console */
 'use strict';
+import fsp from 'fs/promises';
+
+async function copyFile() {
+  const argv = process.argv;
+  const source = argv[2];
+  const destination = argv[3];
+
+  if (source !== destination) {
+    try {
+      await fsp.cp(source, destination);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+}
+
+copyFile();
