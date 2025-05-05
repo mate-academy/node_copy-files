753bdc7d46f2cb55f6a69aeb4736eb1b44d555e8
Vasyl Pylypchynets <pylypchynets.vasyl@gmail.com>
2025-05-05 11:04:01 +0300

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,32 @@
+/* eslint-disable max-len */
+/* eslint-disable no-console */
 'use strict';
+
+const fs = require('fs');
+
+async function copyFile() {
+  const [source, destination] = process.argv.slice(2);
+
+  if (!source || !destination) {
+    console.error(
+      'Invalid command. Please provide exactly 2 arguments: <source> <destination>',
+    );
+
+    return;
+  }
+
+  if (source === destination) {
+    console.error('Source and destination cannot be the same.');
+
+    return;
+  }
+
+  try {
+    await fs.promises.copyFile(source, destination);
+    console.log(`File copied from ${source} to ${destination}`);
+  } catch (error) {
+    console.error(`Error copying file: ${error.message}`);
+  }
+}
+
+copyFile();
