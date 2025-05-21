8d3a1a3f7d36564c510d12498a929278829cfe08
Sofiia Pelenska <soniapelenskaoe2002@gmail.com>
2025-05-22 01:16:01 +0300

    Solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,38 @@
 'use strict';
+import fs from 'fs';
+
+const copyFile = () => {
+  try {
+    const [moveFrom, moveTo] = process.argv.slice(2);
+
+    if (!moveFrom || !moveTo) {
+      // eslint-disable-next-line no-console
+      console.error('Input all arguments');
+
+      return;
+    }
+
+    fs.readFile(moveFrom, 'utf8', (error, data) => {
+      if (error) {
+        // eslint-disable-next-line no-console
+        console.error('Can not read the file', error.message);
+
+        return;
+      }
+
+      fs.writeFile(moveTo, data, 'utf8', (err) => {
+        if (err) {
+          // eslint-disable-next-line no-console
+          console.error('Can not write the file', err.message);
+        }
+      });
+    });
+    // eslint-disable-next-line no-console
+    console.log('Success!');
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.error(e.message);
+  }
+};
+
+copyFile();
