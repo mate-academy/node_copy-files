1e73488d8cd3bce1c49682bafa1f38db5e8d09c5
AVyhaa <artemvyhovanec1@gmail.com>
2025-05-21 17:01:22 +0300

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,25 @@
 'use strict';
+
+const fs = require('fs/promises');
+
+const [moveFrom, moveTo] = process.argv.slice(2);
+
+if (!moveFrom || !moveTo) {
+  // eslint-disable-next-line no-console
+  console.error(
+    'The "path" argument must be of type string or an' +
+      'instance of Buffer or URL.',
+  );
+} else if (moveFrom !== moveTo) {
+  fs.readFile(moveFrom, 'utf-8')
+    .then((data) => {
+      fs.writeFile(moveTo, data).catch((error) => {
+        // eslint-disable-next-line no-console
+        console.error(error);
+      });
+    })
+    .catch((error) => {
+      // eslint-disable-next-line no-console
+      console.error(error);
+    });
+}
