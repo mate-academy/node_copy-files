518525fed0c7d0519ffd957911ff9d474ee322ce
fromjkeee16 <fromjkeee16@gmail.com>
2025-06-04 19:16:44 +0300

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,15 @@
+/* eslint-disable no-console */
 'use strict';
+
+const fs = require('node:fs');
+const args = process.argv.slice(2);
+const [sourcePath, destinationPath] = args;
+
+try {
+  const source = fs.readFileSync(sourcePath);
+  const content = source.toString();
+
+  fs.writeFileSync(destinationPath, content);
+} catch (e) {
+  console.error(e);
+}
