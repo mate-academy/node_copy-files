b94fa208cab0d9124bc0b15c45355e9333943be1
Nadiia Bulmak <nadgaval@gmail.com>
2025-06-08 16:40:40 +0300

    solution

--- src/app.js
+++ src/app.js
@@ -3,19 +3,27 @@
 
 const fs = require('node:fs');
 
-const [sourcePath, destinationPath] = process.argv.slice(2);
+function copyFile() {
+  const [sourcePath, destinationPath] = process.argv.slice(2);
 
-if (process.argv.length !== 5) {
-  console.error('Error: Please provide source and destination file paths');
+  if (!sourcePath || !destinationPath) {
+    console.error('Error: Please provide source and destination file paths');
+
+    return;
+  }
+
+  if (sourcePath === destinationPath) {
+    console.error('Error: Source and destination paths are the same');
+
+    return;
+  }
+
+  try {
+    fs.copyFileSync(sourcePath, destinationPath);
+    console.log(`File copied from ${sourcePath} to ${destinationPath}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+  }
 }
 
-if (sourcePath === destinationPath) {
-  console.error('Error: Source and destination paths are the same');
-}
-
-try {
-  fs.copyFileSync(sourcePath, destinationPath);
-  console.log(`File copied from ${sourcePath} to ${destinationPath}`);
-} catch (error) {
-  console.error(`Error: ${error.message}`);
-}
+copyFile();
