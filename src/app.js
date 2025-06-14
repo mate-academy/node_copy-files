3925dbd7eac8a6d5cdffb80e0c6da2ac70aaba45
Tania Kuzmenko <tanch.sergeevich@gmail.com>
2025-06-14 19:36:23 +0300

    Solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,44 @@
 'use strict';
+
+const fs = require('fs');
+const path = require('path');
+
+const args = process.argv.slice(2);
+const [src, dest] = args;
+
+if (!src || !dest) {
+  // eslint-disable-next-line no-console
+  console.error('sorce or destination is not defined');
+  process.exit();
+}
+
+const resolvedSrc = path.resolve(src);
+const resolvedDest = path.resolve(dest);
+
+if (resolvedSrc === resolvedDest) {
+  process.exit();
+}
+
+fs.stat(resolvedSrc, (err, stats) => {
+  if (err) {
+    // eslint-disable-next-line no-console
+    console.error('Error reading source file:', err);
+    process.exit();
+  }
+
+  if (!stats.isFile()) {
+    // eslint-disable-next-line no-console
+    console.error('Source is not a file');
+    process.exit();
+  }
+
+  fs.copyFile(resolvedSrc, resolvedDest, (er) => {
+    if (er) {
+      // eslint-disable-next-line no-console
+      console.error('Error copying file:', er);
+      process.exit();
+    }
+    // eslint-disable-next-line no-console
+    console.log('File copied successfully!');
+  });
+});
