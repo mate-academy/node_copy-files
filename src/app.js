7faec707f5b62e8257b27e1751954f89d552d020
Vitalii Shchukin <135863695+VitaliiShc@users.noreply.github.com>
2025-06-27 00:50:34 +0300

    Solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,45 @@
+/* eslint-disable no-console */
 'use strict';
+
+const fs = require('fs');
+const path = require('path');
+
+function copyFile() {
+  const copyArgs = process.argv.slice(2);
+
+  if (copyArgs.length !== 2) {
+    console.error('Script accepts 2 arguments: sourse and destination');
+
+    return;
+  }
+
+  const [sourceFile, destinationFile] = copyArgs;
+
+  if (sourceFile === destinationFile) {
+    return;
+  }
+
+  const fileName = path.basename(destinationFile);
+  const destinationDirName = path.dirname(destinationFile);
+  const sourseDirName = path.dirname(sourceFile);
+
+  if (!fs.existsSync(sourceFile)) {
+    console.error('Source file is not exist');
+
+    return;
+  }
+
+  try {
+    fs.copyFileSync(sourceFile, destinationFile);
+
+    console.log(
+      `File ${fileName} copied from ${sourseDirName} to ${destinationDirName}`,
+    );
+  } catch (err) {
+    console.error(`Error: ${err.message}`);
+  }
+}
+
+copyFile();
+
+module.exports = { copyFile };
