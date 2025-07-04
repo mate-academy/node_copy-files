7d07a7e3083839fcd66016e5defba52863551703
Stas Borychevskyi <sborichevskiy74@gmail.com>
2025-07-04 18:33:57 +0300

    Solution 1.0

--- src/app.js
+++ src/app.js
@@ -1,1 +1,24 @@
 'use strict';
+
+const fs = require('fs');
+const params = process.argv.slice(2);
+
+const makeCopy = (sourcePath, destinationPath) => {
+  if (sourcePath === destinationPath) {
+    // eslint-disable-next-line no-console
+    console.error('That is the same route');
+  }
+
+  try {
+    fs.copyFileSync(sourcePath, destinationPath);
+    // eslint-disable-next-line no-console
+    console.log('Success!');
+  } catch (err) {
+    // eslint-disable-next-line no-console
+    console.error(err);
+  }
+};
+
+module.exports = { makeCopy };
+
+makeCopy(params[0], params[1]);
