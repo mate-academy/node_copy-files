18e68d3ab8f561fd1a29868ee0fd52b9526b1043
Vitalii Hutsalo <jacklounter07@gmail.com>
2025-07-08 13:44:29 +0300

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,43 @@
 'use strict';
+
+const fs = require('fs');
+const path = require('path');
+
+function copyFile(sourcePath, destinationPath) {
+  const src = path.resolve(sourcePath);
+  const dest = path.resolve(destinationPath);
+
+  if (src === dest) {
+    return;
+  }
+
+  if (!fs.existsSync(src)) {
+    // eslint-disable-next-line no-console
+    console.error('Error: source file does not exist');
+
+    return;
+  }
+
+  if (!fs.statSync(src).isFile()) {
+    // eslint-disable-next-line no-console
+    console.error('Error: source is not a file');
+
+    return;
+  }
+
+  try {
+    fs.copyFileSync(src, dest);
+  } catch (err) {
+    // eslint-disable-next-line no-console
+    console.error('Error copying file:', err.message);
+  }
+}
+
+const args = process.argv.slice(2);
+
+if (args.length !== 2) {
+  // eslint-disable-next-line no-console
+  console.error('Node error');
+} else {
+  copyFile(args[0], args[1]);
+}
