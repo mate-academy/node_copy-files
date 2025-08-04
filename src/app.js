ba3d7ccfb3fc8b5ac063d615b955d845e071c201
Oleh Mysko <olegmysko1996@gmail.com>
2025-08-04 16:52:32 +0300

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,27 @@
+/* eslint-disable no-console */
 'use strict';
+
+function copyFile() {
+  const fs = require('fs');
+  const arg = process.argv.slice(2);
+
+  if (arg.length !== 2) {
+    console.error('must be a two argument');
+
+    return;
+  }
+
+  if (arg[0] === arg[1]) {
+    console.error('copy file must be a new directory');
+
+    return;
+  }
+
+  fs.copyFile(arg[0], arg[1], (error) => {
+    if (error) {
+      console.error('copy faile do not found', error);
+    }
+  });
+}
+
+copyFile();
