113d9d3bcd14062c4b4dba96c0f7d4d548176172
Kostiantyn Shestakov <shestakov.kostiantyn@gmail.com>
2025-08-21 16:06:34 +0300

    add solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,18 @@
 'use strict';
+
+import fs from 'fs';
+
+function copyFile() {
+  const [copyFrom, copyTo] = process.argv.slice(2);
+
+  try {
+    const copyFromData = fs.readFileSync(copyFrom).toString();
+
+    fs.writeFileSync(copyTo, copyFromData);
+  } catch (err) {
+    // eslint-disable-next-line
+    console.error(err);
+  }
+}
+
+copyFile();
