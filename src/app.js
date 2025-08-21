00c2c602d585e3b7372e00b6f6bcbb21f598c2e3
Kostiantyn Shestakov <shestakov.kostiantyn@gmail.com>
2025-08-21 17:52:45 +0300

    solution

--- src/app.js
+++ src/app.js
@@ -1,29 +1,39 @@
 'use strict';
 
 const fs = require('fs');
+const path = require('path');
 
 async function copyFile() {
   const [copyFrom, copyTo] = process.argv.slice(2);
 
   if (!copyFrom || !copyTo) {
     // eslint-disable-next-line
-    console.error('One of values is null');
+    console.error('Error: missing source or destination file');
 
     return;
   }
 
-  if (copyFrom === copyTo) {
-    // eslint-disable-next-line
-    console.error('Same paths');
+  const copyFromPath = path.resolve(copyFrom);
+  const copyToPath = path.resolve(copyTo);
 
+  if (copyFromPath === copyToPath) {
     return;
   }
 
   try {
-    await fs.promises.copyFile(copyFrom, copyTo);
+    const stat = await fs.promises.stat(copyFromPath);
+
+    if (!stat.isFile()) {
+      // eslint-disable-next-line
+      console.error('Error: source is not a file');
+
+      return;
+    }
+
+    await fs.promises.copyFile(copyFromPath, copyToPath);
   } catch (err) {
     // eslint-disable-next-line
-    console.error(err.message);
+    console.error(`Error: ${err.message}`);
   }
 }
 
