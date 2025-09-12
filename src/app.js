0806fec96dbaf044307c6e25d28b860fbc5711d8
VladSlob <141657806+VladSlob@users.noreply.github.com>
2025-09-12 09:27:42 +0300

    app.js

--- src/app.js
+++ src/app.js
@@ -5,10 +5,6 @@ import fs from 'fs';
   try {
     const file = fs.readFileSync(inputFile);
 
-    fs.writeFileSync(outputFile, file);
-  } catch (err) {
-    console.error(err);
-  }
 }
 
 copyFile(source, destination);
