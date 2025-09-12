5aebe679be227f8484a2560e953fa16890082c34
VladSlob <141657806+VladSlob@users.noreply.github.com>
2025-09-12 09:32:22 +0300

    app.js

--- src/app.js
+++ src/app.js
@@ -2,9 +2,16 @@
 'use strict';
 import fs from 'fs';
 
+const [source, destination] = process.argv.slice(2);
+
+function copyFile(inputFile, outputFile) {
   try {
     const file = fs.readFileSync(inputFile);
 
+    fs.writeFileSync(outputFile, file);
+  } catch (err) {
+    console.error(err);
+  }
 }
 
 copyFile(source, destination);
