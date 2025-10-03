47ce73f6ff43da1fb7a6c7d35b0de6e5563c1c2b
Viktoriia <viktoriamyhailiak@gmail.com>
2025-10-03 20:21:48 +0300

    add a solution

--- src/app.js
+++ src/app.js
@@ -1,54 +1,65 @@
 'use strict';
 
-const fs = require('fs');
-const path = require('path');
-const args = process.argv.slice(2);
+import fs from 'fs';
 
-function App(original, copy) {
-  if (args.length !== 2) {
-    return;
-  }
-
-  if (!original || !copy) {
+function copy() {
+  if (process.argv.slice(2).length !== 2) {
     // eslint-disable-next-line no-console
-    console.error('Both source and destination paths must be provided');
+    console.error(
+      'Wrong number of arguments is supplied, should pass 2 arguments',
+    );
 
     return;
   }
 
-  if (path.resolve(original) === path.resolve(copy)) {
+  const [sourceFile, destinationFile] = process.argv.slice(2);
+
+  if (!sourceFile || !destinationFile) {
+    // eslint-disable-next-line no-console
+    console.error('One or two param is undefined');
+
     return;
+  }
+
+  if (sourceFile === destinationFile) {
+    return;
+  }
+
+  if (!fs.existsSync(sourceFile)) {
+    // eslint-disable-next-line no-console
+    console.error('Non-existent source file');
+
+    return;
+  }
+
+  const sourceStats = fs.statSync(sourceFile);
+
+  if (sourceStats.isDirectory()) {
+    // eslint-disable-next-line no-console
+    console.error('Source is a directory');
+
+    return;
+  }
+
+  if (fs.existsSync(destinationFile)) {
+    const destStats = fs.statSync(destinationFile);
+
+    if (destStats.isDirectory()) {
+      // eslint-disable-next-line no-console
+      console.error('Destination is a directory');
+
+      return;
+    }
   }
 
   try {
-    const stats = fs.statSync(original);
-
-    if (!stats.isFile()) {
-      // eslint-disable-next-line no-console
-      console.error('Source must be a file');
-
-      return;
-    }
-
-    if (fs.existsSync(copy) && fs.statSync(copy).isDirectory()) {
-      // eslint-disable-next-line no-console
-      console.error('Destination cannot be a directory');
-
-      return;
-    }
-
-    fs.copyFileSync(original, copy);
+    fs.copyFileSync(sourceFile, destinationFile);
   } catch (err) {
     // eslint-disable-next-line no-console
-    console.error(err.message);
+    console.error(err);
   }
 }
 
-if (args.length === 2) {
-  App(args[0], args[1]);
-} else {
-  // eslint-disable-next-line no-console
-  console.error('You need to pass 2 arguments!');
-}
+copy();
 
-module.exports = { App };
+export default copy;
