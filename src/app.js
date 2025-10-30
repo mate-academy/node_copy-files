35804fa5994d47bc456c2eea3cef85a6f8e1ddf7
Саша <martinecsasa5@gmail.com>
2025-10-30 20:42:31 +0200

    add task solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,57 @@
 'use strict';
+
+const fs = require('fs');
+
+function copy() {
+  if (process.argv.slice(2).length !== 2) {
+    // eslint-disable-next-line no-console
+    console.error('Потрібно 2 аргументи');
+
+    return;
+  }
+
+  const [srcFile, destCopy] = process.argv.slice(2);
+
+  if (!srcFile || !destCopy) {
+    // eslint-disable-next-line no-console
+    console.error('Аргументи є undefined');
+  }
+
+  if (!fs.existsSync(srcFile)) {
+    // eslint-disable-next-line no-console
+    console.error('Файл не існує');
+
+    return;
+  }
+
+  const sourceStats = fs.statSync(srcFile);
+
+  if (sourceStats.isDirectory()) {
+    // eslint-disable-next-line no-console
+    console.error('джерело є каталогом');
+
+    return;
+  }
+
+  if (fs.existsSync(srcFile)) {
+    const destStats = fs.statSync(srcFile);
+
+    if (destStats.isDirectory()) {
+      // eslint-disable-next-line no-console
+      console.error('призначення є каталогом');
+
+      return;
+    }
+  }
+
+  try {
+    fs.copyFileSync(srcFile, destCopy);
+  } catch (err) {
+    // eslint-disable-next-line no-console
+    console.error(err);
+  }
+}
+
+copy();
+
+module.exports = copy;
