c9520178db564bb484836f2db2fc38c7215a7386
Vouchyk <user@users-MacBook-Air.local>
2025-12-20 22:46:32 +0000

    Tasks Solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,31 @@
+/* eslint-disable no-console */
 'use strict';
+
+const { copyFile } = require('fs/promises');
+
+async function app() {
+  const [source, destination] = process.argv.slice(2);
+
+  if (!source || !destination) {
+    /* eslint-disable-next-line no-console */
+    console.error('Provide source and destination');
+
+    return;
+  }
+
+  if (source === destination) {
+    return;
+  } else {
+    try {
+      await copyFile(source, destination);
+    } catch (e) {
+      console.error(`The file could not be copied. Error: ${e}`);
+    }
+  }
+
+  process.exit(0);
+}
+
+app();
+
+module.exports = { app };
