a09118dfe8de4c9639bf030017f22f1cf3f7fbff
Kyrylo Golikov <pipecc1985@gmail.com>
2025-12-28 14:15:19 +0100

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,25 @@
 'use strict';
+
+const fs = require('fs');
+const path = require('path');
+
+function cp(src, dest) {
+  const srcPath = path.resolve(src);
+  const destPath = path.resolve(dest);
+
+  if (srcPath === destPath) {
+    // eslint-disable-next-line no-useless-return
+    return;
+  }
+
+  fs.copyFileSync(srcPath, destPath);
+}
+
+try {
+  const [src, dest] = process.argv.slice(2);
+
+  cp(src, dest);
+} catch (error) {
+  // eslint-disable-next-line no-console
+  console.error('Error copying file:', error.message);
+}
