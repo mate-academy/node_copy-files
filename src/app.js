c9e21d687e70e32fc8f9bf9988c453c8bb188991
Ivan <ivan.kartavy@icloud.com>
2026-01-31 10:42:06 +0100

    add task solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,33 @@
 'use strict';
+
+const fs = require('fs/promises');
+const path = require('path');
+
+async function main() {
+  const [filePath, fileCopyPath] = process.argv.slice(2);
+
+  if (!filePath || !fileCopyPath) {
+    // eslint-disable-next-line no-console
+    console.error(
+      'Please provide both the source file path and the destination file path.',
+    );
+
+    return;
+  }
+
+  const absoluteFilePath = path.resolve(filePath);
+  const absoluteFileCopyPath = path.resolve(fileCopyPath);
+
+  if (absoluteFilePath === absoluteFileCopyPath) {
+    return;
+  }
+
+  try {
+    await fs.copyFile(absoluteFilePath, absoluteFileCopyPath);
+  } catch (err) {
+    // eslint-disable-next-line no-console
+    console.error(err);
+  }
+}
+
+main();
