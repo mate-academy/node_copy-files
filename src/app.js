8ddff1f4751a6a79799f6dd0a4c97154b361b184
Alyona Shunevych <olena17ju06@gmail.com>
2026-02-22 16:50:51 +0200

    Solution

--- src/app.js
+++ src/app.js
@@ -5,10 +5,16 @@ const fs = require('fs');
 
 const [source, dest] = process.argv.slice(2);
 
-if (!source || !dest || source === dest) {
+if (!source || !dest) {
   console.error('Error: parameters are invalid');
-} else {
-  fs.cp(source, dest, (error) => {
-    console.error(error);
+} else if (source !== dest) {
+  fs.copyFile(source, dest, (error) => {
+    if (error) {
+      console.error(error);
+
+      return;
+    }
+
+    console.log('Success');
   });
 }
