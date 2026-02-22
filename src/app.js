72ae23f59c6cff97bba2b96769db56e5c87259b8
Alyona Shunevych <olena17ju06@gmail.com>
2026-02-22 16:36:37 +0200

    Solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,14 @@
+/* eslint-disable no-console */
 'use strict';
+
+const fs = require('fs');
+
+const [source, dest] = process.argv.slice(2);
+
+if (!source || !dest || source === dest) {
+  console.error('Error: parameters are invalid');
+} else {
+  fs.cp(source, dest, (error) => {
+    console.error(error);
+  });
+}
