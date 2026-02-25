9a916b5ae44a6c009250939c05918955c267a638
Victor Komara <vitioklss@gmail.com>
2026-02-25 13:31:24 +0000

    Solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,61 @@
 'use strict';
+
+/* eslint-disable no-console */
+
+const fs = require('fs/promises');
+
+async function copyApp() {
+  const source = process.argv[2];
+  const destination = process.argv[3];
+
+  try {
+    // 1. Перевірка кількості аргументів
+    if (process.argv.length !== 4) {
+      console.error('Source and destination arguments are required.');
+      process.exit(0); // Тести хочуть отримати stderr, але код 0
+    }
+
+    // 2. Якщо шляхи однакові - do nothing
+    if (source === destination) {
+      return;
+    }
+
+    // 3. Перевірка джерела
+    try {
+      const sourceStat = await fs.stat(source);
+
+      if (sourceStat.isDirectory()) {
+        console.error('Source is not a file');
+        process.exit(0);
+      }
+    } catch (err) {
+      if (err.code === 'ENOENT') {
+        console.error('Source file does not exist');
+        process.exit(0);
+      }
+      throw err;
+    }
+
+    // 4. Перевірка цілі
+    try {
+      const destStat = await fs.stat(destination);
+
+      if (destStat.isDirectory()) {
+        console.error('Destination is a directory');
+        process.exit(0);
+      }
+    } catch (err) {
+      if (err.code !== 'ENOENT') {
+        throw err;
+      }
+    }
+
+    // 5. Копіювання
+    await fs.copyFile(source, destination);
+  } catch (err) {
+    console.error(err.message);
+    process.exit(0);
+  }
+}
+
+copyApp();
