e14047734ed6d77464caa88b310ceafcc3f535ec
tvk777 <klimenko.tv7@gmail.com>
2026-03-11 19:23:18 +0200

    add solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,50 @@
+/* eslint-disable no-console  */
 'use strict';
+
+const fs = require('fs/promises');
+const path = require('path');
+
+// console.error('Error checking file:', error.message);
+
+const copyMyFile = async () => {
+  const [source, dest] = process.argv.slice(2);
+
+  if (!source || !dest) {
+    console.error('Source and destination paths are required');
+
+    return;
+  }
+
+  if (path.resolve(source) === path.resolve(dest)) {
+    console.error('Source and Destination are the same');
+
+    return;
+  }
+
+  try {
+    const stats = await fs.stat(source);
+
+    if (!stats.isFile()) {
+      console.error('Source is not a file');
+
+      return;
+    }
+
+    await fs.copyFile(source, dest);
+    console.log('File copied successfully!');
+  } catch (error) {
+    if (error.code === 'ENOENT') {
+      console.error(`File ${source} not found!`);
+    } else {
+      console.error(error.message);
+    }
+  }
+};
+
+copyMyFile();
+
+/*
+should throw an error if only one argument is provided(128 ms)
+should throw an error if source is a directory(130 ms)
+'./src/source.txt'  './src/destination.txt'
+*/
