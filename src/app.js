8a3c931b485e4a18502550fbf351ce193e1a5b42
Nazarii-Lesniak <naziksay@gmail.com>
2026-03-18 17:03:31 +0200

    silution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,31 @@
+/* eslint-disable no-console */
 'use strict';
+
+const fs = require('fs/promises');
+const path = require('path');
+
+const args = process.argv.slice(2);
+const [source, destination] = args;
+
+async function copy() {
+  if (!source || !destination) {
+    console.error('Please specify the source and destination file paths');
+
+    return;
+  }
+
+  const sourcePath = path.resolve(source);
+  const destinationPath = path.resolve(destination);
+
+  if (sourcePath === destinationPath) {
+    return;
+  }
+
+  try {
+    await fs.copyFile(sourcePath, destinationPath);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+copy();
