cfce527f3e69102f14a1722b936804108761c871
Yan Kazymyr <kazymyr.yan@gmail.com>
2026-03-31 22:36:03 +0300

    Solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const fs = require('fs');
+
+function copyFiles() {
+  const [source, destination] = process.argv.slice(2);
+
+  try {
+    if (source === destination) {
+      return;
+    }
+
+    fs.copyFileSync(source, destination);
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error(error.message);
+  }
+}
+
+copyFiles();
