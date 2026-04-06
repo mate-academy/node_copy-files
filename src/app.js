427204746a1624ca4519f4f62faca03107694287
Dmytro Halieba <dmytro.halyeba@gmail.com>
2026-04-06 18:31:48 +0300

    feat: copy file function

--- src/app.js
+++ src/app.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const fs = require('fs');
+const path = require('path');
+
+const [src, dest] = process.argv.slice(2);
+
+const srcPath = path.resolve(src);
+const destPath = path.resolve(dest);
+
+if (srcPath !== destPath) {
+  try {
+    fs.copyFileSync(src, dest);
+  } catch (err) {
+    console.error(err);
+  }
+}
