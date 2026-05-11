096ae52b0f840fa422ca6289c1e2619383e48845
Tetiana Sobolieva <tetiana.sobolieva.s@gmail.com>
2026-05-11 15:40:47 +0300

    Solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,42 @@
 'use strict';
+
+const fs = require('fs');
+const path = require('path');
+
+const [, , path1, path2] = process.argv;
+
+if (!path1 || !path2) {
+  /* eslint-disable no-console */
+
+  console.error(new Error('Invalid arguments!'));
+  process.exit();
+}
+
+const pathFrom = path.resolve(path1);
+const pathTo = path.resolve(path2);
+
+if (pathFrom === pathTo) {
+  process.exit();
+}
+
+try {
+  const sourceStat = fs.statSync(pathFrom);
+
+  if (!sourceStat.isFile()) {
+    throw new Error('Source is not a file');
+  }
+
+  if (fs.existsSync(pathTo)) {
+    const destinationStat = fs.statSync(pathTo);
+
+    if (!destinationStat.isFile()) {
+      throw new Error('Source is not a file');
+    }
+  }
+
+  const data = fs.readFileSync(pathFrom);
+
+  fs.writeFileSync(pathTo, data);
+} catch (error) {
+  console.error(error);
+}
