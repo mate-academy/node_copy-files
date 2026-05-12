cde5964c4f00f77d12e594a9b5f1997bed94d590
Mykhailo Podaniev <mpodaniev@gmail.com>
2026-05-12 08:47:35 +0300

    Add file copying functionality with error handling

--- src/app.js
+++ src/app.js
@@ -1,1 +1,45 @@
 'use strict';
+
+const fs = require('fs');
+
+(() => {
+  const sourceFilePath = process.argv[2];
+  const destinationFilePath = process.argv[3];
+
+  if (process.argv.length !== 4) {
+    // eslint-disable-next-line no-console
+    console.error('Usage: node app.js <sourceFilePath> <destinationFilePath>');
+
+    return;
+  }
+
+  if (sourceFilePath === destinationFilePath) {
+    return;
+  }
+
+  if (!fs.existsSync(sourceFilePath)) {
+    // eslint-disable-next-line no-console
+    console.error('Source file does not exist');
+
+    return;
+  }
+
+  if (fs.statSync(sourceFilePath).isDirectory()) {
+    // eslint-disable-next-line no-console
+    console.error('Source is a directory');
+
+    return;
+  }
+
+  if (
+    fs.existsSync(destinationFilePath) &&
+    fs.statSync(destinationFilePath).isDirectory()
+  ) {
+    // eslint-disable-next-line no-console
+    console.error('Destination is a directory');
+
+    return;
+  }
+
+  fs.copyFileSync(sourceFilePath, destinationFilePath);
+})();
