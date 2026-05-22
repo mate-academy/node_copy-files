b3863eb2abd163107c5a9a45cf079d80a0f6cd69
Tymur <timurradkevic84@mail.com>
2026-05-22 21:49:04 +0300

    add task solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,11 @@
+/* eslint-disable no-console */
 'use strict';
+import fs from 'fs';
+
+const params = process.argv.slice(2);
+
+if (params.length < 2) {
+  console.log(new Error('You need to write second parameter'));
+} else {
+  fs.cp(params[0], params[1], (error) => console.log(error));
+}
