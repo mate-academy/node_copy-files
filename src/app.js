ef52815c5a4c9c67faa086ce36fa92a5b93763a1
Tymur <timurradkevic84@mail.com>
2026-05-22 21:52:33 +0300

    add task solution

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import fs from 'fs';
 const params = process.argv.slice(2);
 
 if (params.length < 2) {
-  console.log(new Error('You need to write second parameter'));
-} else {
-  fs.cp(params[0], params[1], (error) => console.log(error));
+  console.error(new Error('You need to write second parameter'));
+} else if (params[0] !== params[1]) {
+  fs.cp(params[0], params[1], (error) => console.error(error));
 }
