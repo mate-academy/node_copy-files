f889632af714d1104b3546bf43327168051a1f50
Denys Boiko <big4ever6996@gmail.com>
2026-05-30 16:41:24 +0200

    Solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,25 @@
 'use strict';
+
+const fs = require('fs');
+
+const source = process.argv[2];
+const destination = process.argv[3];
+
+if (source !== destination) {
+  try {
+    if (!source || !destination) {
+      throw new Error('Please provide both source and destination paths.');
+    }
+
+    const stat = fs.statSync(source);
+
+    if (!stat.isFile()) {
+      throw new Error(`The path "${source}" is not a file.`);
+    }
+
+    fs.copyFileSync(source, destination);
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error(error.message);
+  }
+}
