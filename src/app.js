bf41bb3f082b8b4c08005da7a42641acfaf11a3a
Stanislav <fizervitalij897@gmail.com>
2026-06-20 12:45:19 +0300

    add solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,29 @@
-'use strict';
+/* eslint no-console: ["error", { allow: ["warn", "error"] }] */
+
+const fs = require('fs/promises');
+
+const source = process.argv[2];
+const destination = process.argv[3];
+
+if (source === destination) {
+  process.exit(0);
+}
+
+if (!source || !destination) {
+  console.error('Only one argument is provided');
+}
+
+async function copy(from, to) {
+  try {
+    const content = await fs.readFile(`./${from}`, 'utf-8');
+
+    await fs.writeFile(`./${to}`, content);
+  } catch (err) {
+    if (err.code === 'EISDIR') {
+      console.error('You try to use a directory');
+    }
+    console.error(err);
+  }
+}
+
+copy(source, destination);
