1ef99281d6a9aa64e4d782e2554dcab791aa7707
Andriy <andriit35@gmail.com>
2026-07-02 14:16:49 +0300

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,27 @@
 'use strict';
+
+const fs = require('node:fs/promises');
+
+async function main() {
+  const [, , source, dest] = process.argv;
+
+  if (!source || !dest) {
+    // eslint-disable-next-line no-console
+    console.error('Source and destination paths are required');
+
+    return;
+  }
+
+  if (source === dest) {
+    return;
+  }
+
+  try {
+    await fs.copyFile(source, dest);
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error(error);
+  }
+}
+
+main();
