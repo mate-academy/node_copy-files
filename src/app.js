f9f3702811c04e5941e961d9ea3bbc0e9067c731
Kirill <kkoctuk2005@gmail.com>
2026-07-09 19:50:30 +0200

    Solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,48 @@
+/* eslint-disable no-console */
 'use strict';
+
+const fs = require('fs');
+
+function copyFileTo() {
+  const [originPath, destinationPath] = process.argv.slice(2);
+
+  if (!originPath || !destinationPath) {
+    console.error('Only one argument is provided');
+
+    return;
+  }
+
+  if (originPath === destinationPath) {
+    return;
+  }
+
+  if (!fs.existsSync(originPath)) {
+    console.error('Non-existent source file');
+
+    return;
+  }
+
+  fs.stat(originPath, (origError, originStats) => {
+    if (origError) {
+      console.error('Error');
+    }
+
+    if (originStats.isDirectory()) {
+      console.error('Source is a directory');
+    }
+
+    fs.stat(destinationPath, (destError, destStats) => {
+      if (destStats && destStats.isDirectory()) {
+        console.error('Destination is a directory');
+      }
+
+      fs.cp(originPath, destinationPath, (copyError) => {
+        if (copyError) {
+          console.error('Copy Error');
+        }
+      });
+    });
+  });
+}
+
+copyFileTo();
