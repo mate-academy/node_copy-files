d6dd439b7450f60fb04a65351687f5f7d393fcb1
Kirill <kkoctuk2005@gmail.com>
2026-07-09 19:57:15 +0200

    Solution

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ function copyFileTo() {
   const [originPath, destinationPath] = process.argv.slice(2);
 
   if (!originPath || !destinationPath) {
-    console.error('Only one argument is provided');
+    console.error('Two file paths are required');
 
     return;
   }
@@ -25,15 +25,21 @@ function copyFileTo() {
   fs.stat(originPath, (origError, originStats) => {
     if (origError) {
       console.error('Error');
+
+      return;
     }
 
     if (originStats.isDirectory()) {
       console.error('Source is a directory');
+
+      return;
     }
 
     fs.stat(destinationPath, (destError, destStats) => {
       if (destStats && destStats.isDirectory()) {
         console.error('Destination is a directory');
+
+        return;
       }
 
       fs.cp(originPath, destinationPath, (copyError) => {
