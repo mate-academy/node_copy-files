59d34637f3f8e6dfed1994ff6242e6a8b3ccee41
Evgeniy45 <jekaolhovskii@gmail.com>
2026-07-23 18:52:37 +0300

    finished task

--- src/app.js
+++ src/app.js
@@ -1,1 +1,21 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-useless-return */
+/* eslint-disable no-console */
 'use strict';
+import fs from 'fs';
+
+const args = process.argv.slice(2);
+
+export function copyFile(source, dest) {
+  if (source === dest) {
+    return;
+  }
+
+  try {
+    fs.copyFileSync(source, dest);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+copyFile(...args);
