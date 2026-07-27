337616b6c3edb7be80793b4847b0ebc614f029b3
Yana <utrsach@gmail.com>
2026-07-27 22:50:49 +0300

    Solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const fs = require('fs');
+
+const source = process.argv[2];
+const destination = process.argv[3];
+
+if (source === destination) {
+  process.exit(0);
+}
+
+try {
+  fs.copyFileSync(source, destination);
+} catch (error) {
+  // eslint-disable-next-line no-console
+  console.error(error);
+}
