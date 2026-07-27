eb64ef0c97c11747a6765b411813afa85b6f6697
Rostyslav <rooostik2016@gmail.com>
2026-07-27 22:52:28 +0300

    Solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,13 @@
+/* eslint-disable no-console */
 'use strict';
+import { cp } from 'node:fs/promises';
+
+const [from, to] = process.argv.slice(2);
+
+if (from !== to) {
+  try {
+    await cp(from, to);
+  } catch (error) {
+    console.error(error);
+  }
+}
