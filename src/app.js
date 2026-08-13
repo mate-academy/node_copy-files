f7160b5a6eb755c48dfa3d67c8385ab275cc296b
linska <linskatanya@gmail.com>
2026-08-13 16:54:42 +0300

    feat: add copy file logic

--- src/app.js
+++ src/app.js
@@ -1,1 +1,27 @@
+/* eslint-disable no-console */
+
 'use strict';
+
+import fsp from 'node:fs/promises';
+
+async function main() {
+  const argv = process.argv.slice(2);
+
+  if (argv.length < 2 || argv[0] === argv[1]) {
+    return;
+  }
+
+  const [filePath, filePathCopy] = argv;
+
+  try {
+    const fileData = await fsp.readFile(filePath, 'utf8');
+
+    await fsp.writeFile(filePathCopy, fileData).catch((error) => {
+      console.log(error);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+main();
