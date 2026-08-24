e3dd2bb4ed451c4c9efc462a96253457bac4afec
Roman <yamshchikovr11@gmail.com>
2026-08-24 17:46:03 +0300

    add task solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,47 @@
 'use strict';
+
+import fs from 'fs';
+
+const params = process.argv.slice(2);
+const [userFile, fileCopy] = params;
+
+function main() {
+  if (userFile === fileCopy) {
+    // eslint-disable-next-line no-console
+    console.error('These addres files is the same');
+
+    return;
+  }
+
+  if (!userFile) {
+    // eslint-disable-next-line no-console
+    console.error('Plesse writed adres for file');
+
+    return;
+  }
+
+  if (!fileCopy) {
+    // eslint-disable-next-line no-console
+    console.error('Plesse writed adres for copy file');
+
+    return;
+  }
+
+  fs.readFile(userFile, 'utf-8', (error, data) => {
+    if (error) {
+      // eslint-disable-next-line no-console
+      console.error(error);
+
+      return;
+    }
+
+    fs.writeFile(fileCopy, data, (errors) => {
+      if (errors) {
+        // eslint-disable-next-line no-console
+        console.error(errors);
+      }
+    });
+  });
+}
+
+main();
